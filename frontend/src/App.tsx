2f08cfef1526f35541a217a9a9054a235ea772b3
Irving Zamora <irvingzamora28@gmail.com>
2023-04-09 00:48:20 -0600

    Create LoginPage component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
+import LoginPage from './components/LoginPage'
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">
-      Hello world!
-    </h1>
+    <div>
+        <LoginPage />
+    </div>
   )
 }
 
