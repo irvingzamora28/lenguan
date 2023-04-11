29e159234b80d0e12e90d9e7f182ee4b97b806fb
Irving Zamora <irvingzamora28@gmail.com>
2023-04-11 01:33:38 -0600

    Handle game-over event in ColorButtons component

--- frontend/src/components/ColorButtons.tsx
+++ frontend/src/components/ColorButtons.tsx
@@ -1,71 +1,67 @@
 // ColorButtons.tsx
-import React, { useState, useEffect } from 'react';
-import socket from './../../socket-server/socket';
+import React, { useState, useEffect } from "react";
+import socket from "./../../socket-server/socket";
 
-const colors = ['Red', 'Blue', 'Green', 'Yellow'];
+const colors = ["Red", "Blue", "Green", "Yellow"];
 
 const getRandomColor = () => {
-  const randomIndex = Math.floor(Math.random() * colors.length);
-  return colors[randomIndex];
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex];
 };
 
 const ColorButtons: React.FC = () => {
-  const [currentColor, setCurrentColor] = useState(getRandomColor());
-  const [score, setScore] = useState({ player1: 0, player2: 0 });
+    const [currentColor, setCurrentColor] = useState(getRandomColor());
+    const [score, setScore] = useState({ player1: 0, player2: 0 });
+    const [gameStatus, setGameStatus] = useState<string | null>(null);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentColor(getRandomColor());
-    }, 10000);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentColor(getRandomColor());
+        }, 10000);
 
-    socket.on('update-score', (updatedScore) => {
-      setScore(updatedScore);
-    });
+        socket.on("update-score", (updatedScore) => {
+            setScore(updatedScore);
+        });
 
-    return () => {
-      clearInterval(interval);
-      socket.off('update-score');
+        socket.on("game-over", (message: string) => {
+            setGameStatus(message);
+        });
+
+        return () => {
+            clearInterval(interval);
+            socket.off("update-score");
+            socket.off("game-over");
+        };
+    }, []);
+
+    const handleButtonClick = (color: string) => {
+        console.log(`${color} button clicked`);
+        if (color === currentColor) {
+            socket.emit("correct-color-clicked", color);
+        }
     };
-  }, []);
 
-  const handleButtonClick = (color: string) => {
-    console.log(`${color} button clicked`)
-    if (color === currentColor) {
-      socket.emit('correct-color-clicked', color);
-    }
-  };
-
-  return (
-    <div className="flex flex-col items-center">
-      <h1 className="text-4xl font-bold mb-8">Press {currentColor} button</h1>
-      <div className="flex flex-wrap justify-center">
-        <button
-          className="bg-red-500 w-32 h-32 m-4 rounded-lg focus:outline-none hover:bg-red-600"
-          onClick={() => handleButtonClick('Red')}
-        ></button>
-        <button
-        className="bg-blue-500 w-32 h-32 m-4 rounded-lg focus:outline-none hover:bg-blue-600"
-                  onClick={() => handleButtonClick('Blue')}
-
-      ></button>
-      <button
-        className="bg-green-500 w-32 h-32 m-4 rounded-lg focus:outline-none hover:bg-green-600"
-                  onClick={() => handleButtonClick('Green')}
-
-      ></button>
-      <button
-        className="bg-yellow-500 w-32 h-32 m-4 rounded-lg focus:outline-none hover:bg-yellow-600"
-                  onClick={() => handleButtonClick('Yellow')}
-
-      ></button>
-        {/* Add the same for other buttons */}
-      </div>
-      <div className="mt-8">
-        <p>Player 1: {score.player1}</p>
-        <p>Player 2: {score.player2}</p>
-      </div>
-    </div>
-  );
+    return (
+        <div className="flex flex-col items-center">
+            <h1 className="text-4xl font-bold mb-8">
+                {gameStatus ? gameStatus : `Press ${currentColor} button`}
+            </h1>
+            <div className="flex flex-wrap justify-center">
+                {colors.map((color) => (
+                    <button
+                        key={color}
+                        className={`bg-${color.toLowerCase()}-500 w-32 h-32 m-4 rounded-lg focus:outline-none hover:bg-${color.toLowerCase()}-600`}
+                        onClick={() => handleButtonClick(color)}
+                        disabled={!!gameStatus}
+                    ></button>
+                ))}
+            </div>
+            <div className="mt-8">
+                <p>Player 1: {score.player1}</p>
+                <p>Player 2: {score.player2}</p>
+            </div>
+        </div>
+    );
 };
 
 export default ColorButtons;
