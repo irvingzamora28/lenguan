01853ca26989f198c3bf6c75798325e7b911ede2
Irving Zamora <irvingzamora28@gmail.com>
2023-04-10 23:36:52 -0600

    Add vite config to use port 3000 for the frontend app

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
+// vite.config.js
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    manifest: true,
+    outDir: 'public/assets',
+    rollupOptions: {
+      input: 'resources/js/app.tsx',
+    },
+  },
   plugins: [react()],
-})
+  server: {
+    host: 'localhost',
+    port: 3000,
+    strictPort: true,
+    proxy: {
+      '*': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        ws: true,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'resources/js'),
+    },
+  },
+});
