5863588376745b02973fa9d4dab7695baa724033
Irving Zamora <irvingzamora28@gmail.com>
2023-04-20 00:52:18 -0600

    Add react-chartjs-2 chart.js packages Add ChartsCard component to DashboardPage

---
+++ frontend/src/components/DashboardPage/Cards/ChartsCard.tsx
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, ChartData, ChartOptions } from "chart.js";
+import { Line } from "react-chartjs-2";
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+
+const VocabularyChartCard: React.FC = () => {
+	const [activeTab, setActiveTab] = useState<number>(0);
+
+	// Sample data for the line chart
+	const chartData1: ChartData<"line"> = {
+		labels: ["JUN-01", "JUN-02", "JUN-03", "JUN-04", "JUN-05", "JUN-06", "JUN-07"],
+		datasets: [
+			{
+				label: "Number of words learned",
+				data: [2, 5, 7, 10, 15, 20, 25],
+				backgroundColor: "rgba(255, 99, 132, 0.2)",
+				borderColor: "rgba(255, 99, 132, 1)",
+				borderWidth: 1,
+			},
+		],
+	};
+
+	const chartData2: ChartData<"line"> = {
+		labels: ["MAY-28", "MAY-29", "JAN-01", "JAN-03", "JAN-04", "JAN-06", "JAN-07"],
+		datasets: [
+			{
+				label: "Number of points earned",
+				data: [13, 33, 48, 12, 10, 20, 34],
+				backgroundColor: "rgba(54, 162, 235, 0.2)",
+				borderColor: "rgba(54, 162, 235, 1)",
+				borderWidth: 1,
+			},
+		],
+	};
+
+	// Chart options
+	const chartOptions1: ChartOptions<"line"> = {
+		responsive: true,
+		plugins: {
+			legend: {
+				position: "top" as const,
+			},
+			title: {
+				display: true,
+				text: "Words Learned",
+			},
+		},
+	};
+
+    const chartOptions2: ChartOptions<"line"> = {
+		responsive: true,
+		plugins: {
+			legend: {
+				position: "top" as const,
+			},
+			title: {
+				display: true,
+				text: "Points Earned Last 10 Days played",
+			},
+		},
+	};
+
+	return (
+		<div className="col-span-1 md:col-span-2 h-fit bg-white p-6 rounded-xl drop-shadow-2xl">
+			<h3 className="text-2xl font-bold mb-4">Progress</h3>
+			<div className="tabs">
+				<ul className="flex">
+					<li className={`-mb-px mr-1 ${activeTab === 0 ? "border-blue-500 border-b-2" : "border-gray-200"}`}>
+						<a className={`bg-white inline-block cursor-pointer py-2 px-4 font-semibold ${activeTab === 0 ? "text-blue-500" : "text-gray-500"} hover:text-blue-800`} onClick={() => setActiveTab(0)}>
+							Vocabulary
+						</a>
+					</li>
+					<li className={`-mb-px mr-1 ${activeTab === 1 ? "border-blue-500 border-b-2" : "border-gray-200"}`}>
+						<a className={`bg-white inline-block cursor-pointer py-2 px-4 font-semibold ${activeTab === 1 ? "text-blue-500" : "text-gray-500"} hover:text-blue-800`} onClick={() => setActiveTab(1)}>
+							Gender Duel
+						</a>
+					</li>
+				</ul>
+			</div>
+			<div className="tab-content">
+				<div className={`tab-item ${activeTab === 0 ? "block" : "hidden"}`}>
+					<Line data={chartData1} options={chartOptions1} />
+				</div>
+				<div className={`tab-item ${activeTab === 1 ? "block" : "hidden"}`}>
+					<Line data={chartData2} options={chartOptions2} />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default VocabularyChartCard;
--- frontend/src/components/DashboardPage/DashboardPage.tsx
+++ frontend/src/components/DashboardPage/DashboardPage.tsx
@@ -6,6 +6,7 @@ import Navbar from "./Navbar/Navbar";
 import SocialFeedCard from "./Cards/SocialFeedCard";
 import LeaderboardCard from "./Cards/LeaderboardCard";
 import FriendsCard from "./Cards/FriendsCard";
+import ChartsCard from "./Cards/ChartsCard";
 
 const DashboardPage: React.FC = () => {
 	const [profileOpen, setProfileOpen] = useState(false);
@@ -20,36 +21,7 @@ const DashboardPage: React.FC = () => {
 					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita quam odit officiis magni doloribus ipsa dolore, dolores nihil accusantium labore, incidunt autem iure quae vitae voluptate, esse asperiores aliquam
 					repellat. Harum aliquid non officiis porro at cumque eaque inventore iure. Modi sunt optio mollitia repellat sed ab quibusdam quos harum!
 					<section className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 mt-10 gap-8">
-						{/* Card 1 */}
-						<div className="bg-blue-500 p-8 rounded-xl text-gray-300 flex flex-col gap-6">
-							<RiLineChartLine className="text-5xl" />
-							<h4 className="text-2xl">Earnings</h4>
-							<span className="text-5xl text-white">&euro; 8,350</span>
-							<span className="py-1 px-3 bg-blue-500/80 rounded-full">+ 10% since last month</span>
-						</div>
-						{/* Card 2 */}
-						<div className="p-4 bg-white rounded-xl flex flex-col justify-between gap-4 drop-shadow-2xl">
-							<div className="flex items-center gap-4 bg-blue-500/10 rounded-xl p-4">
-								<span className="bg-blue-500 text-gray-300 text-2xl font-bold p-4 rounded-xl">98</span>
-								<div>
-									<h3 className="font-bold">Rank</h3>
-									<p className="text-gray-500">In top 30%</p>
-								</div>
-							</div>
-							<div className="bg-blue-500/10 rounded-xl p-4">
-								<div className="flex items-center gap-4 mb-4">
-									<span className="bg-blue-500 text-gray-300 text-2xl font-bold p-4 rounded-xl">32</span>
-									<div>
-										<h3 className="font-bold">Projects</h3>
-										<p className="text-gray-500">8 this month</p>
-									</div>
-								</div>
-								<div className="flex items-center gap-2 text-gray-500 text-sm">
-									<span className="bg-blue-500/20 py-1 px-4 rounded-full">Mobile app</span>
-									<span className="bg-blue-500/20 py-1 px-4 rounded-full">Branding</span>
-								</div>
-							</div>
-						</div>
+                        <ChartsCard />
                         <FriendsCard />
 					</section>
 					<section className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 mt-10 gap-8">
