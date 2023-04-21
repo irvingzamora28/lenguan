e1026325e0edd7374cec7565a55465f4dcfa84ab
Irving Zamora <irvingzamora28@gmail.com>
2023-04-20 23:39:12 -0600

    Add correct and incorrect audio assets Play correct and incorrect sound effects when user clicks Gender buttons

--- frontend/src/components/GenderDuelPage/GenderDuelPage.tsx
+++ frontend/src/components/GenderDuelPage/GenderDuelPage.tsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import socket from "./../../../socket-server/socket";
+import correctSound from "../../assets/audio/correct-choice.mp3";
+import incorrectSound from "../../assets/audio/incorrect-choice.mp3";
 
 const genders = [
 	{
@@ -41,88 +43,102 @@ const words = [
 
 const getRandomWord = () => {
 	const randomIndex = Math.floor(Math.random() * words.length);
-    console.log(`random word: ${words[randomIndex].word}`);
-
 	return words[randomIndex];
 };
 
 type Word = {
-    word: string;
-    gender: string;
-    translation: string;
-    difficulty_level: number;
-    category: string;
-  };
+	word: string;
+	gender: string;
+	translation: string;
+	difficulty_level: number;
+	category: string;
+};
 
+type GameStatus = {
+	message: string;
+	onGoing: boolean;
+	sound: string | null;
+};
 
 const GenderDuelPage: React.FC = () => {
-    const [currentWord, setCurrentWord] = useState<Word | null>(null);
-    const [score, setScore] = useState({ player1: 0, player2: 0 });
-    const [gameStatus, setGameStatus] = useState<string | null>(null);
-    const [playerNumber, setPlayerNumber] = useState<number | null>(null);
-    const [intervalId, setIntervalId] = useState<number | NodeJS.Timer | undefined>(undefined);
-    const [correctGender, setCorrectGender] = useState<string | null>(null);
+	const [currentWord, setCurrentWord] = useState<Word | null>(null);
+	const [score, setScore] = useState({ player1: 0, player2: 0 });
+    const [gameStatus, setGameStatus] = useState<GameStatus>({message:"", onGoing:true, sound:null});
+	const [playerNumber, setPlayerNumber] = useState<number | null>(null);
+	const [intervalId, setIntervalId] = useState<number | NodeJS.Timer | undefined>(undefined);
+	const [correctGender, setCorrectGender] = useState<string | null>(null);
 
-    useEffect(() => {
-        setCurrentWord(getRandomWord());
-        const interval = setInterval(() => {
-            setCurrentWord(getRandomWord());
-        }, 10000);
-        setIntervalId(interval);
+	useEffect(() => {
+		if (gameStatus.sound) {
+			const audio = new Audio(gameStatus.sound);
+			audio.play();
+		}
+        setGameStatus({ message: ``, onGoing: true, sound: null });
+	}, [gameStatus.sound]);
 
-        socket.on("player-assignment", (player: number) => {
-            setPlayerNumber(player);
-        });
+	useEffect(() => {
+		setCurrentWord(getRandomWord());
+		const interval = setInterval(() => {
+			setCurrentWord(getRandomWord());
+		}, 10000);
+		setIntervalId(interval);
 
-        socket.on("update-score", (updatedScore) => {
-            setScore(updatedScore);
-        });
+		socket.on("player-assignment", (player: number) => {
+			setPlayerNumber(player);
+		});
 
-        socket.on("game-over", (message: string) => {
-            setGameStatus(message);
-        });
+		socket.on("update-score", (updatedScore) => {
+			setScore(updatedScore);
+		});
 
-        return () => {
-            clearInterval(interval);
-            socket.off("player-assignment");
-            socket.off("update-score");
-            socket.off("game-over");
-        };
-    }, []);
+		socket.on("game-over", (message: string) => {
+			setGameStatus({message: message, onGoing:false, sound: correctSound});
+		});
 
-    const handleButtonClick = (gender: string) => {
-        console.log(`${gender} button clicked`);
-        if (currentWord && gender === currentWord.gender) {
-            socket.emit("correct-gender-clicked", gender);
-            setCorrectGender(gender);
-            clearInterval(intervalId);
+		return () => {
+			clearInterval(interval);
+			socket.off("player-assignment");
+			socket.off("update-score");
+			socket.off("game-over");
+		};
+	}, []);
 
-            setTimeout(() => {
+	const handleButtonClick = (gender: string) => {
+		console.log(`${gender} button clicked`);
+		if (currentWord && gender === currentWord.gender) {
+			socket.emit("correct-gender-clicked", gender);
+			setCorrectGender(gender);
+			clearInterval(intervalId);
+			setTimeout(() => {
                 setCurrentWord(getRandomWord());
-
-                const interval = setInterval(() => {
-                    setCurrentWord(getRandomWord());
-                    setCorrectGender(null);
-                }, 10000);
                 console.log(`new word: ${currentWord?.word}`);
-                setIntervalId(interval);
-            }, 1000);
-        }
-    };
 
+				const interval = setInterval(() => {
+					setCurrentWord(getRandomWord());
+					setCorrectGender(null);
+				}, 10000);
+				console.log(`new word: ${currentWord?.word}`);
+				setIntervalId(interval);
+			}, 1000);
+            setGameStatus({ message: `Correct! The gender of "${currentWord.word}" is "${gender}"`, onGoing: true, sound: correctSound });
+		} else {
+            console.log(`Incorrect`);
+			setGameStatus({ message: `Incorrect! The gender of "${currentWord?.word}" is not "${gender}".`, onGoing: true, sound: incorrectSound });
+		}
+	};
 
 	return (
 		<div className="flex flex-col items-center">
 			<div className="d-none bg-green-500 bg-red-500 bg-blue-500 bg-yellow-500"></div>
 			{playerNumber !== null && <h2 className="text-2xl font-semibold mb-4">{playerNumber === 0 ? "Spectator" : `Player ${playerNumber}`}</h2>}
-			<h1 className="text-4xl font-bold mb-8">{gameStatus ? gameStatus : `Press the correct gender for ${currentWord?.word}`}</h1>
+			<h1 className="text-4xl font-bold mb-8">{!gameStatus.onGoing ? gameStatus.message : `Press the correct gender for ${currentWord?.word}`}</h1>
 			<div className="flex flex-wrap justify-center">
 				{genders.map((gender) => (
 					<button
 						key={gender.color}
-                        className={`bg-${gender.color.toLowerCase()}-500 w-32 h-32 m-4 rounded-lg focus:outline-none hover:bg-${gender.color.toLowerCase()}-600 ${correctGender === gender.name.toLowerCase() ? "animate-bounce" : ""}`}
+						className={`bg-${gender.color.toLowerCase()}-500 w-32 h-32 m-4 rounded-lg focus:outline-none hover:bg-${gender.color.toLowerCase()}-600 ${correctGender === gender.name.toLowerCase() ? "animate-bounce" : ""}`}
 						onClick={() => handleButtonClick(gender.name.toLowerCase())}
-						disabled={!!gameStatus}
+						disabled={!gameStatus.onGoing}
 					>
 						{gender.name}
 					</button>
