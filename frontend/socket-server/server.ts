78c9987c6b2d4e67fa5cca35e84280748b9ec0e9
Irving Zamora <irvingzamora28@gmail.com>
2023-04-23 03:18:09 -0600

    Gender Duel: Allow a dynamic number of players

--- frontend/socket-server/server.ts
+++ frontend/socket-server/server.ts
@@ -1,262 +1,265 @@
-// server.ts
 import express from 'express';
-import { Server as HttpServer } from 'http';
-import { Server as SocketIOServer, Socket as BaseSocket } from 'socket.io';
+import {Server as HttpServer} from 'http';
+import {Server as SocketIOServer, Socket as BaseSocket} from 'socket.io';
 
 interface GenderDuelSocket extends BaseSocket {
-  playerNumber?: number;
+    playerNumber?: number;
 }
+
 const app = express();
 const server = new HttpServer(app);
 const io = new SocketIOServer(server, {
-  cors: {
-    origin: '*',
-  },
+    cors: {
+        origin: '*'
+    }
 });
 
+const MAX_PLAYERS = 2;
+
 interface Score {
-  player1: number;
-  player2: number;
+    [key : string]: number;
 }
 
 interface GameState {
-  players: string[];
-  score: Score;
+    players: string[];
+    score: Score;
 }
 
 const gameState: GameState = {
-  players: [],
-  score: { player1: 0, player2: 0 },
+    players: [],
+    score: {}
 };
 
 let intervalId: NodeJS.Timeout;
 
-const availablePlayerNumbers = [1, 2];
+const availablePlayerNumbers = Array.from({
+    length: MAX_PLAYERS
+}, (_, i) => i + 1);
 
 const words = [
-	{
-		word: "Haus",
-		gender: "das",
-		translation: "house",
-		difficulty_level: 1,
-		category: "Housing",
-	},
-	{
-		word: "Apfel",
-		gender: "der",
-		translation: "apple",
-		difficulty_level: 1,
-		category: "Food & Drinks",
-	},
-	{
-		word: "Katze",
-		gender: "die",
-		translation: "cat",
-		difficulty_level: 1,
-		category: "Animals",
-	},
-	{
-		word: "Auto",
-		gender: "das",
-		translation: "car",
-		difficulty_level: 1,
-		category: "Transportation",
-	},
-	{
-		word: "Baum",
-		gender: "der",
-		translation: "tree",
-		difficulty_level: 1,
-		category: "Nature",
-	},
-	{
-		word: "Maus",
-		gender: "die",
-		translation: "mouse",
-		difficulty_level: 2,
-		category: "Animals",
-	},
-	{
-		word: "Wasser",
-		gender: "das",
-		translation: "water",
-		difficulty_level: 1,
-		category: "Food & Drinks",
-	},
-	{
-		word: "Kuh",
-		gender: "die",
-		translation: "cow",
-		difficulty_level: 2,
-		category: "Animals",
-	},
-	{
-		word: "Stuhl",
-		gender: "der",
-		translation: "chair",
-		difficulty_level: 1,
-		category: "Furniture",
-	},
-	{
-		word: "Flugzeug",
-		gender: "das",
-		translation: "airplane",
-		difficulty_level: 2,
-		category: "Transportation",
-	},
-	{
-		word: "Hund",
-		gender: "der",
-		translation: "dog",
-		difficulty_level: 1,
-		category: "Animals",
-	},
-	{
-		word: "Tisch",
-		gender: "der",
-		translation: "table",
-		difficulty_level: 1,
-		category: "Furniture",
-	},
-	{
-		word: "Schaf",
-		gender: "das",
-		translation: "sheep",
-		difficulty_level: 2,
-		category: "Animals",
-	},
-	{
-		word: "Buch",
-		gender: "das",
-		translation: "book",
-		difficulty_level: 1,
-		category: "Education",
-	},
-	{
-		word: "Fahrrad",
-		gender: "das",
-		translation: "bicycle",
-		difficulty_level: 2,
-		category: "Transportation",
-	},
-	{
-		word: "Fenster",
-		gender: "das",
-		translation: "window",
-		difficulty_level: 1,
-		category: "Housing",
-	},
-	{
-		word: "Vogel",
-		gender: "der",
-		translation: "bird",
-		difficulty_level: 1,
-		category: "Animals",
-	},
-	{
-		word: "Kleid",
+    {
+        word: "Haus",
         gender: "das",
-		difficulty_level: 2,
-		translation: "dress",
-		category: "Fashion",
-	},
-	{
-		word: "Eis",
-		gender: "das",
-		translation: "ice cream",
-		difficulty_level: 2,
-		category: "Food & Drinks",
-	},
-	{
-		word: "Spinne",
-		gender: "die",
-		translation: "spider",
-		difficulty_level: 3,
-		category: "Animals",
-	},
+        translation: "house",
+        difficulty_level: 1,
+        category: "Housing"
+    },
+    {
+        word: "Apfel",
+        gender: "der",
+        translation: "apple",
+        difficulty_level: 1,
+        category: "Food & Drinks"
+    },
+    {
+        word: "Katze",
+        gender: "die",
+        translation: "cat",
+        difficulty_level: 1,
+        category: "Animals"
+    },
+    {
+        word: "Auto",
+        gender: "das",
+        translation: "car",
+        difficulty_level: 1,
+        category: "Transportation"
+    }, {
+        word: "Baum",
+        gender: "der",
+        translation: "tree",
+        difficulty_level: 1,
+        category: "Nature"
+    }, {
+        word: "Maus",
+        gender: "die",
+        translation: "mouse",
+        difficulty_level: 2,
+        category: "Animals"
+    }, {
+        word: "Wasser",
+        gender: "das",
+        translation: "water",
+        difficulty_level: 1,
+        category: "Food & Drinks"
+    }, {
+        word: "Kuh",
+        gender: "die",
+        translation: "cow",
+        difficulty_level: 2,
+        category: "Animals"
+    }, {
+        word: "Stuhl",
+        gender: "der",
+        translation: "chair",
+        difficulty_level: 1,
+        category: "Furniture"
+    }, {
+        word: "Flugzeug",
+        gender: "das",
+        translation: "airplane",
+        difficulty_level: 2,
+        category: "Transportation"
+    }, {
+        word: "Hund",
+        gender: "der",
+        translation: "dog",
+        difficulty_level: 1,
+        category: "Animals"
+    }, {
+        word: "Tisch",
+        gender: "der",
+        translation: "table",
+        difficulty_level: 1,
+        category: "Furniture"
+    }, {
+        word: "Schaf",
+        gender: "das",
+        translation: "sheep",
+        difficulty_level: 2,
+        category: "Animals"
+    }, {
+        word: "Buch",
+        gender: "das",
+        translation: "book",
+        difficulty_level: 1,
+        category: "Education"
+    }, {
+        word: "Fahrrad",
+        gender: "das",
+        translation: "bicycle",
+        difficulty_level: 2,
+        category: "Transportation"
+    }, {
+        word: "Fenster",
+        gender: "das",
+        translation: "window",
+        difficulty_level: 1,
+        category: "Housing"
+    }, {
+        word: "Vogel",
+        gender: "der",
+        translation: "bird",
+        difficulty_level: 1,
+        category: "Animals"
+    }, {
+        word: "Kleid",
+        gender: "das",
+        difficulty_level: 2,
+        translation: "dress",
+        category: "Fashion"
+    }, {
+        word: "Eis",
+        gender: "das",
+        translation: "ice cream",
+        difficulty_level: 2,
+        category: "Food & Drinks"
+    }, {
+        word: "Spinne",
+        gender: "die",
+        translation: "spider",
+        difficulty_level: 3,
+        category: "Animals"
+    },
 ];
 
 const getRandomWord = () => {
-	const randomIndex = Math.floor(Math.random() * words.length);
-	return words[randomIndex];
+    const randomIndex = Math.floor(Math.random() * words.length);
+    return words[randomIndex];
 };
 
 const emitNewWord = () => {
     const newWord = getRandomWord();
     io.emit("new-word", newWord);
-    console.log(`New word emitted: ${newWord.word}`);
+    console.log(`New word emitted: ${
+        newWord.word
+    }`);
 
-    // Clear the existing 10-second interval
     clearInterval(intervalId);
-
-    // Reset the 10-second interval
     intervalId = setInterval(() => {
-      if (gameState.players.length === 2) {
-        emitNewWord();
-      }
+        if (gameState.players.length === MAX_PLAYERS) {
+            emitNewWord();
+        }
     }, 10000);
-  };
+};
 
 server.listen(3001, () => {
-  console.log('Socket.IO server is running on port 3001');
+    console.log('Socket.IO server is running on port 3001');
 });
 
-io.on('connection', (socket: GenderDuelSocket) => {
-    console.log(`User connected: ${socket.id}`);
-    console.log(`gameState.players.length: ${gameState.players.length}`);
+io.on('connection', (socket : GenderDuelSocket) => {
+    console.log(`User connected: ${
+        socket.id
+    }`);
+    console.log(`gameState.players.length: ${
+        gameState.players.length
+    }`);
 
     socket.on("start-game", () => {
-        if (gameState.players.length === 2) {
-          io.emit("start-game");
-          emitNewWord();
+        if (gameState.players.length === MAX_PLAYERS) {
+            io.emit("start-game");
+            emitNewWord();
         }
-      });
+    });
 
     if (availablePlayerNumbers.length > 0) {
         const playerNumber = availablePlayerNumbers.shift()!;
         gameState.players.push(socket.id);
+        gameState.score[`player${playerNumber}`] = 0;
         console.log(`Player ${playerNumber} connected`);
         socket.emit("player-assignment", playerNumber);
 
-        // Add player number to the socket object
         socket.playerNumber = playerNumber;
-      } else {
+    } else {
         socket.emit("player-assignment", 0);
-      }
+    }
 
-  socket.on('correct-gender-clicked', (gender: string) => {
-    console.log(`Correct gender clicked ${gender}`);
+    socket.on('correct-gender-clicked', (gender : string) => {
+        console.log(`Correct gender clicked ${gender}`);
 
-    if (gameState.players.length === 2) {
-      const playerIndex = gameState.players.indexOf(socket.id);
-      if (playerIndex !== -1) {
-        gameState.score[`player${playerIndex + 1}` as keyof Score] += 1;
-        if (gameState.score[`player${playerIndex + 1}` as keyof Score] >= 10) {
-          io.emit('game-over', `Player ${playerIndex + 1} wins!`);
-          gameState.score = { player1: 0, player2: 0 };
-        } else {
-          io.emit('update-score', gameState.score);
-          clearInterval(intervalId);
+        if (gameState.players.length === MAX_PLAYERS) {
+            const playerIndex = gameState.players.indexOf(socket.id);
+            if (playerIndex !== -1) {
+                const playerKey = `player${
+                    playerIndex + 1
+                }` as keyof Score;
+                gameState.score[playerKey] += 1;
+                if (gameState.score[playerKey] >= 10) {
+                    io.emit(
+                        'game-over',
+                        `Player ${
+                            playerIndex + 1
+                        } wins!`
+                    );
+                    for (let i = 1; i <= MAX_PLAYERS; i++) {
+                        gameState.score[`player${i}`] = 0;
+                    }
+                } else {
+                    io.emit('update-score', gameState.score);
+                    clearInterval(intervalId);
 
-          // Set the 1-second interval to emit a new word
-          intervalId = setInterval(() => {
-            emitNewWord();
-          }, 1000);
+                    intervalId = setInterval(() => {
+                        emitNewWord();
+                    }, 1000);
+                }
+            }
         }
-      }
-    }
-  });
+    });
 
-  socket.on('disconnect', () => {
-    console.log(`User disconnected: ${socket.id}`);
-    const playerIndex = gameState.players.indexOf(socket.id);
-    if (playerIndex !== -1) {
-      gameState.players.splice(playerIndex, 1);
-      // Reclaim the player number when a player disconnects
-      if (socket.playerNumber) {
-        availablePlayerNumbers.push(socket.playerNumber);
-        availablePlayerNumbers.sort(); // Keep the array sorted
-      }
-    }
-  });
+    socket.on('disconnect', () => {
+        console.log(`User disconnected: ${
+            socket.id
+        }`);
+        const playerIndex = gameState.players.indexOf(socket.id);
+        if (playerIndex !== -1) {
+            gameState.players.splice(playerIndex, 1);
+            if (socket.playerNumber) {
+                availablePlayerNumbers.push(socket.playerNumber);
+                availablePlayerNumbers.sort(); // Keep the array sorted
+            }
+            delete gameState.score[`player${
+                    socket.playerNumber
+                }`];
+        }
+    });
 });
--- frontend/src/components/GenderDuelPage/GenderDuelPage.tsx
+++ frontend/src/components/GenderDuelPage/GenderDuelPage.tsx
@@ -5,6 +5,10 @@ import incorrectSound from "../../assets/audio/incorrect-choice.mp3";
 import "../../assets/scss/components/GenderDuelPage.scss";
 import { FaMars, FaVenus, FaNeuter } from "react-icons/fa";
 
+// type GenderDuelPageProps = {
+//   socket: Socket;
+// }
+
 const genders = [
 	{
 		name: "Der",
@@ -31,23 +35,20 @@ type Word = {
 	category: string;
 };
 
-type GameStatus = {
-	message: string;
-	active: boolean;
-	sound: string | null;
+type Score = {
+	[playerNumber: string]: number;
 };
 
 const GenderDuelPage: React.FC = () => {
-	const [currentWord, setCurrentWord] = useState<Word | null>(null);
-	const [score, setScore] = useState({ player1: 0, player2: 0 });
-	const [gameStatus, setGameStatus] = useState<GameStatus>({ message: "", active: true, sound: null });
-	const [playerNumber, setPlayerNumber] = useState<number | null>(null);
+	const [word, setWord] = useState<Word | null>(null);
+	const [score, setScore] = useState<Score>({});
+	const [playerNumber, setPlayerNumber] = useState(0);
 	const [correctGender, setCorrectGender] = useState<string | null>(null);
 	const [incorrectGender, setIncorrectGender] = useState<string | null>(null);
+	const [gameStatus, setGameStatus] = useState("waiting");
 	const [appearing, setAppearing] = useState(false);
-	const [gameStarted, setGameStarted] = useState(false);
-	const [waitingPlayer, setWaitingPlayer] = useState<number | null>(null);
 	const speechSynthesisRef = useRef<SpeechSynthesisUtterance | null>(null);
+    const [soundEffect, setSoundEffect] = useState<string | null>(null)
 
 	const resetAnimation = () => {
 		setCorrectGender(null);
@@ -55,130 +56,126 @@ const GenderDuelPage: React.FC = () => {
 		setAppearing(false);
 	};
 
-
-	useEffect(() => {
-		if (gameStatus.sound) {
-			const audio = new Audio(gameStatus.sound);
+    useEffect(() => {
+		if (soundEffect) {
+			const audio = new Audio(soundEffect);
 			audio.play();
 		}
-		setGameStatus({ message: ``, active: true, sound: null });
-	}, [gameStatus.sound]);
+        setSoundEffect(null)
+	}, [soundEffect]);
 
 	useEffect(() => {
-		socket.on("start-game", () => {
-			setGameStarted(true);
+		socket.on("player-assignment", (assignedPlayerNumber: number) => {
+			setPlayerNumber(assignedPlayerNumber);
 		});
 
 		socket.on("new-word", (newWord: Word) => {
-			setCurrentWord(newWord);
-			setAppearing(true);
+			setWord(newWord);
 		});
 
-		socket.on("player-assignment", (player: number) => {
-			setPlayerNumber(player);
-			if (player === 1) {
-				setWaitingPlayer(2);
-			} else if (player === 2) {
-				setWaitingPlayer(1);
-			}
-		});
-
-		socket.on("update-score", (updatedScore) => {
+		socket.on("update-score", (updatedScore: Score) => {
 			setScore(updatedScore);
 		});
 
+		socket.on("start-game", () => {
+			setGameStatus("playing");
+		});
+
 		socket.on("game-over", (message: string) => {
-			setGameStatus({ message: message, active: false, sound: correctSound });
+			setGameStatus("game-over");
+			alert(message);
 		});
 
 		return () => {
-			socket.off("start-game");
 			socket.off("player-assignment");
 			socket.off("new-word");
 			socket.off("update-score");
+			socket.off("start-game");
 			socket.off("game-over");
-			socket.off("both-players-connected");
 		};
-	}, []);
+	}, [socket]);
 
-	const handleButtonClick = (gender: string) => {
+	const handleGenderClick = (gender: string) => {
 		console.log(`${gender} button clicked`);
-		if (currentWord && gender === currentWord.gender) {
+		if (word && word.gender === gender) {
 			socket.emit("correct-gender-clicked", gender);
 			setCorrectGender(gender);
-
-			setGameStatus({ message: ``, active: true, sound: correctSound });
+            setSoundEffect(correctSound)
 
 			if ("speechSynthesis" in window) {
-				const utterance = new SpeechSynthesisUtterance(`${gender} ${currentWord.word}`);
+                const utterance = new SpeechSynthesisUtterance(`${gender} ${word.word}`);
 				speechSynthesisRef.current = utterance;
 				utterance.lang = "de-DE";
 				utterance.rate = 0.8;
 				speechSynthesis.speak(utterance);
 			} else {
-				console.log("Text-to-speech not supported in this browser");
+                console.log("Text-to-speech not supported in this browser");
 			}
 		} else {
-			console.log(`Incorrect`);
+            console.log(`Incorrect`);
 			setIncorrectGender(gender);
-			setGameStatus({ message: ``, active: true, sound: incorrectSound });
+            setSoundEffect(incorrectSound)
 		}
 	};
 
-	const handleStartButtonClick = () => {
-		console.log("Start button clicked");
-		console.log("playerNumber: ", playerNumber);
-
+	const handleStartGame = () => {
 		socket.emit("start-game");
 	};
 
 	return (
 		<div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-t from-blue-400 to-blue-100">
-			{!gameStarted && (
-				<>
-					<span className="gender_duel__subtitle font-semibold m-4 text-white">PLAYER {playerNumber}</span>
-					<button
-						className="flex items-center shadow-box justify-center h-24 w-64 drop-shadow-xl rounded-lg px-8 py-4 overflow-hidden group bg-yellow-400 relative hover:bg-gradient-to-r hover:from-yellow-400 hover:to-yellow-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-yellow-400 transition-all ease-out duration-300"
-						onClick={handleStartButtonClick}
-					>
-						<span className="absolute right-0 w-12 h-44 -mt-12 transition-all duration-1000 transform translate-x-16 bg-white opacity-10 rotate-12 group-hover:-translate-x-72 ease"></span>
-						<span className="relative gender_duel__text-shadow font-bold">START</span>
-					</button>
-					{waitingPlayer !== null ? <p className="text-2xl font-semibold m-4 text-white">Player {waitingPlayer} waiting for you</p> : <p className="text-white">Waiting for the opponent...</p>}
-				</>
-			)}
-			{gameStarted && (
-				<>
-					<div className="d-none bg-red-500 active:bg-red-700 from-red-400 to-red-500"></div>
-					<div className="d-none bg-blue-500 active:bg-blue-700 from-blue-400 to-blue-500"></div>
-					<div className="d-none bg-green-500 active:bg-green-700 from-green-400 to-green-500"></div>
-					{playerNumber !== null && <h2 className="text-2xl font-semibold mb-4 text-white">{playerNumber === 0 ? "Spectator" : `Player ${playerNumber}`}</h2>}
-					<h1 className={`gender_duel__title ${appearing ? "animate-appear" :  ""}`} onAnimationEnd={resetAnimation}>
-						{!gameStatus.active ? gameStatus.message : `${currentWord?.word}`}
-					</h1>
-					<div className="flex flex-wrap justify-center w-4/5">
-						{genders.map((gender) => (
-							<button
-								key={gender.color}
-								className={`${
-									correctGender === gender.name.toLowerCase() ? "animate-grow" : correctGender !== null && correctGender !== gender.name.toLowerCase() ? "hidden" : ""
-								} bg-${gender.color.toLowerCase()}-500 w-16 h-16 md:w-32 md:h-32 m-4 rounded-lg focus:outline-none hover:bg-${gender.color.toLowerCase()}-600 hover:shadow-lg active:bg-${gender.color.toLowerCase()}-700 active:scale-95 ${
-									correctGender === gender.name.toLowerCase() ? "animate-correct" : incorrectGender === gender.name.toLowerCase() ? "animate-incorrect" : ""
-								} transition duration-300 ease-in-out shadow-box bg-gradient-to-t from-${gender.color.toLowerCase()}-400 to-${gender.color.toLowerCase()}-500`}
-								onClick={() => handleButtonClick(gender.name.toLowerCase())}
-								onAnimationEnd={resetAnimation}
-								disabled={!gameStatus.active}
-							>
-								<div className="flex flex-row justify-center items-center text-lg md:text-4xl">
-									{gender.icon}
-									<span className="font-semibold">{gender.name}</span>
-								</div>
-							</button>
-						))}
-					</div>
+			{gameStatus !== "playing" && <h1 className="gender_duel__title text-center">Gender Duel</h1>}
+			{playerNumber === 0 ? (
+				<p>Game is full. Please wait for an available slot.</p>
+			) : (
+                <>
+                {playerNumber !== null && <h2 className="gender_duel__subtitle font-semibold m-4 text-white">{playerNumber === 0 ? "Spectator" : `Player ${playerNumber}`}</h2>}
+					{gameStatus === "waiting" && (
+						<button
+							className="flex items-center shadow-box justify-center h-24 w-64 drop-shadow-xl rounded-lg px-8 py-4 overflow-hidden group bg-yellow-400 relative hover:bg-gradient-to-r hover:from-yellow-400 hover:to-yellow-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-yellow-400 transition-all ease-out duration-300"
+							onClick={handleStartGame}
+						>
+							<span className="absolute right-0 w-12 h-44 -mt-12 transition-all duration-1000 transform translate-x-16 bg-white opacity-10 rotate-12 group-hover:-translate-x-72 ease"></span>
+							<span className="relative gender_duel__text-shadow font-bold">START</span>
+						</button>
+					)}
+					{gameStatus === "playing" && word && (
+						<>
+							<div className="d-none bg-red-500 active:bg-red-700 from-red-400 to-red-500"></div>
+							<div className="d-none bg-blue-500 active:bg-blue-700 from-blue-400 to-blue-500"></div>
+							<div className="d-none bg-green-500 active:bg-green-700 from-green-400 to-green-500"></div>
+							<h1 className={`gender_duel__title text-center ${appearing ? "animate-appear" : ""}`} onAnimationEnd={resetAnimation}>
+								{gameStatus !== "playing" ? gameStatus : `${word.word} (${word.translation})`}
+							</h1>
+							<div className="flex flex-wrap justify-center w-4/5">
+								{genders.map((gender) => (
+									<button
+										key={gender.color}
+										className={`${
+											correctGender === gender.name.toLowerCase() ? "animate-grow" : correctGender !== null && correctGender !== gender.name.toLowerCase() ? "hidden" : ""
+										} bg-${gender.color.toLowerCase()}-500 w-16 h-16 md:w-32 md:h-32 m-4 rounded-lg focus:outline-none hover:bg-${gender.color.toLowerCase()}-600 hover:shadow-lg active:bg-${gender.color.toLowerCase()}-700 active:scale-95 ${
+											correctGender === gender.name.toLowerCase() ? "animate-correct" : incorrectGender === gender.name.toLowerCase() ? "animate-incorrect" : ""
+										} transition duration-300 ease-in-out shadow-box bg-gradient-to-t from-${gender.color.toLowerCase()}-400 to-${gender.color.toLowerCase()}-500`}
+										onClick={() => handleGenderClick(gender.name.toLowerCase())}
+										onAnimationEnd={resetAnimation}
+										disabled={gameStatus !== "playing"}
+									>
+										<div className="flex flex-row justify-center items-center text-lg md:text-4xl">
+											{gender.icon}
+											<span className="font-semibold">{gender.name}</span>
+										</div>
+									</button>
+								))}
+							</div>
+						</>
+					)}
 					<div className="mt-8 text-white">
-						<p className="font-semibold">Player 1: {score.player1}</p>
-						<p className="font-semibold">Player 2: {score.player2}</p>
+						<h2 className="">Score</h2>
+						{Object.entries(score).map(([player, playerScore]) => (
+							<p key={player} className="font-semibold">
+								{player}: {playerScore}
+							</p>
+						))}
 					</div>
 				</>
 			)}
