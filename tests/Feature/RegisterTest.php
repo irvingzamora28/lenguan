c3a1c7dae33aa6b79b18b46f548c72e929d2dd20
Irving Zamora <irvingzamora28@gmail.com>
2023-04-27 00:16:55 -0600

    Make UserFactory behave like the AuthController when it registers a user Add all tests, correct and add PHPDocs to the RegisterTest

--- tests/Feature/RegisterTest.php
+++ tests/Feature/RegisterTest.php
@@ -3,236 +3,194 @@
 namespace Tests\Feature;
 
 use App\Models\User;
-use Database\Factories\UserFactory;
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Validation\ValidationException;
 use Tests\TestCase;
-use function Tests\createUser;
 
+/**
+ * Unit test class for user registration functionality.
+ *
+ * @package     App\Tests\Unit
+ */
 class RegisterTest extends TestCase
 {
-    use WithFaker, DatabaseMigrations;
+    /**
+     * Trait providing the Faker library to generate random data for testing.
+     */
+    use WithFaker;
 
+    /**
+     * Trait for running database migrations before each test.
+     */
+    use DatabaseMigrations;
+
+    /**
+     * Set up the test environment before each test.
+     */
     protected function setUp(): void
     {
         parent::setUp();
 
         $this->withoutExceptionHandling(); // Disable exception handling for easier debugging
 
+        // Add middleware to verify CSRF token
         $this->app->make(Kernel::class)->pushMiddleware(VerifyCsrfToken::class);
     }
 
+    /**
+     * Test user registration with valid input data.
+     */
     public function testValidRegistration(): void
     {
-
-
+        // Generate random user data
         $name = $this->faker->name;
         $email = $this->faker->unique()->safeEmail;
         $password = $this->faker->regexify('[A-Za-z0-9]{8}');
-
         $data = [
             'name' => $name,
             'email' => $email,
             'password' => $password,
             'password_confirmation' => $password,
         ];
-        $token = csrf_token();
-        $headers = ['X-XSRF-TOKEN' => $token]; // Set the CSRF token in the headers
-        // dd(csrf_token());
-        // $data = [
-        //     'name' => "John Doe123",
-        //     'email' => "johndoe123@email.com",
-        //     'password' => "password",
-        //     'password_confirmation' => "password",
-        // ];
-        // $user = User::factory()->create([
-        //     'name' => 'John Doe',
-        //     'email' => 'john@example.com',
-        //     'password' => "password",
-        //     'password_confirmation' => "password",
-        // ]);
 
-        // // Check if the user record exists in the database
-        // $this->assertDatabaseHas('users', [
-        //     '_id' => $user->_id,
-        //     'name' => 'John Doe',
-        //     'email' => 'john@example.com',
-        //     'password' => "password",
-        //     'password_confirmation' => "password",
-        // ]);
-        // $user = User::factory()->create();
-        // $this->assertDatabaseCount('users', 1);
-
-        // $this->assertDatabaseHas('users', [
-        //     "_id" => $user->_id,
-        //     'name' => $user->name,
-        //     'email' => $user->email,
-        //     'password' => $user->password,
-        //     'password_confirmation' => $user->password
-        // ]);
-        $response = $this->withHeaders($headers)->postJson('/api/register', $data);
-        // dd($response->getContent());
+        // Make a POST request to the registration endpoint and check the response
+        $response = $this->postJson('/api/register', $data);
         $response->assertStatus(200)
             ->assertJson(['message' => 'User registered successfully. Please check your email to verify your account.']);
-        // dd($response);
-        // $user = User::first();
-        // dd($user->username);
-        // dd(count($users));
-        // Create a user record using the UserFactory
-        // $user = User::factory()->create($data);
 
-        // Check if the user record exists in the database
-        // $this->assertDatabaseHas('users', array_merge([ 'id' => $user->id], $data));
-        // $user = UserFactory::new()->of(User::class)->create($data);
-        // $this->assertDatabaseCount('users', 1);
-        // $this->assertDatabaseHas('users', [
-        //     'name' => $name,
-        //     'email' => $email,
-        // ]);
-
-        // dd($email);
+        // Check that a username was generated for the user with the correct format
         $user = User::where('email', $email)->first();
-        $users = User::all();
-        dd(count($users));
         $this->assertNotNull($user->username);
         $this->assertStringContainsString(strstr($email, '@', true), $user->username);
         $this->assertNotEquals($email, $user->username . '@mail.com');
         $this->assertMatchesRegularExpression('/^' . preg_quote(strstr($email, '@', true), '/') . '\d*$/', $user->username);
-
     }
 
-    // public function testNonUniqueEmail(): void
-    // {
-    //     $existingUser = User::factory()->create();
+    /**
+     * Test user registration with non-unique email input data.
+     */
+    public function testNonUniqueEmail(): void
+    {
+        // Generate random user data with an email that already exists in the database
+        $existingUser = User::factory()->create();
+        $data = [
+            'name' => $this->faker->name,
+            'email' => $existingUser->email,
+            'password' => $this->faker->password,
+            'password_confirmation' => $this->faker->password,
+        ];
 
-    //     $data = [
-    //         'name' => $this->faker->name,
-    //         'email' => $existingUser->email,
-    //         'password' => $this->faker->password,
-    //         'password_confirmation' => $this->faker->password,
-    //     ];
+        // Make a POST request to the registration endpoint and check the response
+        $response = $this->postJson('/api/register', $data);
+        $response->assertStatus(422)
+            ->assertJsonValidationErrors(['email']);
+    }
 
-    //     $response = $this->postJson('/api/register', $data);
+    /**
+     * Test user registration with missing name input data.
+     */
+    public function testMissingNameField(): void
+    {
+        // Generate random user data with a missing name field
+        $data = [
+            'email' => $this->faker->unique()->safeEmail,
+            'password' => $this->faker->password,
+            'password_confirmation' => $this->faker->password,
+        ];
 
-    //     $response->assertStatus(422)
-    //              ->assertJsonValidationErrors(['email']);
-    // }
+        // Make a POST request to the registration endpoint and check the response
+        $response = $this->postJson('/api/register', $data);
+        $response->assertStatus(422)
+            ->assertJsonValidationErrors(['name']);
+    }
 
-    // public function testMissingNameField(): void
-    // {
-    //     $data = [
-    //         'email' => $this->faker->unique()->safeEmail,
-    //         'password' => $this->faker->password,
-    //         'password_confirmation' => $this->faker->password,
-    //     ];
+    /**
+     * Test user registration with invalid email format input data.
+     */
+    public function testInvalidEmailFormat(): void
+    {
+        // Generate random user data with an invalid email format
+        $password = $this->faker->regexify('[A-Za-z0-9]{8}');
+        $data = [
+            'name' => $this->faker->name,
+            'email' => 'invalid-email-format',
+            'password' => $password,
+            'password_confirmation' => $password,
+        ];
 
-    //     $response = $this->postJson('/api/register', $data);
+        // Make a POST request to the registration endpoint and check the response
+        $response = $this->postJson('/api/register', $data);
+        $response->assertStatus(422)
+            ->assertJsonValidationErrors(['email']);
+    }
 
-    //     $response->assertStatus(422)
-    //              ->assertJsonValidationErrors(['name']);
-    // }
+    /**
+     * Test user registration with a short password input data.
+     */
+    public function testShortPassword(): void
+    {
+        // Generate random user data with a short password
+        $data = [
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'password' => 'short',
+            'password_confirmation' => 'short',
+        ];
 
-    // public function testInvalidEmailFormat(): void
-    // {
-    //     $data = [
-    //         'name' => $this->faker->name,
-    //         'email' => 'invalid-email-format',
-    //         'password' => $this->faker->password,
-    //         'password_confirmation' => $this->faker->password,
-    //     ];
+        // Make a POST request to the registration endpoint and check the response
+        $response = $this->postJson('/api/register', $data);
+        $response->assertStatus(422)
+            ->assertJsonValidationErrors(['password']);
+    }
 
-    //     $response = $this->postJson('/api/register', $data);
+    /**
+     * Test user registration with non-matching passwords input data.
+     */
+    public function testNonMatchingPasswords(): void
+    {
+        // Generate random user data with non-matching passwords
+        $data = [
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'password' => $this->faker->password,
+            'password_confirmation' => 'does-not-match',
+        ];
 
-    //     $response->assertStatus(422)
-    //              ->assertJsonValidationErrors(['email']);
-    // }
+        // Make a POST request to the registration endpoint and check the response
+        $response = $this->postJson('/api/register', $data);
+        $response->assertStatus(422)
+            ->assertJsonValidationErrors(['password']);
+    }
 
-    // public function testShortPassword(): void
-    // {
-    //     $data = [
-    //         'name' => $this->faker->name,
-    //         'email' => $this->faker->unique()->safeEmail,
-    //         'password' => 'short',
-    //         'password_confirmation' => 'short',
-    //     ];
+    /**
+     * Test user registration with an existing username generated with an existing email but with a different domain input data.
+     */
+    public function testExistingUsernameGeneratedWithExistingEmailWithDifferentDomain(): void
+    {
+        // Generate random user data with an email that already exists in the database but with a different domain
+        $existingUser = User::factory()->create();
+        $existingUsername = $existingUser->username;
+        $existingEmailDifferentDomain = str_replace('@', '@sameemaildifferentdomain.com', $existingUser->email);
+        $password = $this->faker->regexify('[A-Za-z0-9]{8}');
+        $data = [
+            'name' => $this->faker->name,
+            'email' => $existingEmailDifferentDomain,
+            'password' => $password,
+            'password_confirmation' => $password,
+        ];
 
-    //     $response = $this->postJson('/api/register', $data);
+        // Make a POST request to the registration endpoint and check the response
+        $response = $this->postJson('/api/register', $data);
+        $response->assertStatus(200)
+            ->assertJson(['message' => 'User registered successfully. Please check your email to verify your account.']);
 
-    //     $response->assertStatus(422)
-    //              ->assertJsonValidationErrors(['password']);
-    // }
-
-    // public function testNonMatchingPasswords(): void
-    // {
-
-
-    //     $data = [
-    //         'name' => $this->faker->name,
-    //         'email' => $this->faker->unique()->safeEmail,
-    //         'password' => $this->faker->password,
-    //         'password_confirmation' => 'does-not-match',
-    //     ];
-
-    //     $response = $this->postJson('/api/register', $data);
-
-    //     $response->assertStatus(422)
-    //              ->assertJsonValidationErrors(['password']);
-    // }
-
-    // public function testExistingUsername(): void
-    // {
-    //     $existingUser = User::factory()->create();
-    //     $existingUsername = str_replace('@', '', $existingUser->email);
-
-    //     $data = [
-    //         'name' => $this->faker->name,
-    //         'email' => $existingUser->email,
-    //         'password' => $this->faker->password,
-    //         'password_confirmation' => $this->faker->password,
-    //     ];
-
-    //     $response = $this->postJson('/api/register', $data);
-
-    //     $response->assertStatus(200)
-    //              ->assertJson(['message' => 'User registered successfully. Please check your email to verify your account.']);
-
-    //     $this->assertDatabaseHas('users', [
-    //         'name' => $data['name'],
-    //         'email' => $data['email'],
-    //         'username' => $existingUsername . '1',
-    //     ]);
-    // }
-
-    // public function testExistingUsernameWithMultipleUsers(): void
-    // {
-    //     $existingUser1 = User::factory()->create();
-    //     $existingUsername = str_replace('@', '', $existingUser1->email);
-
-    //     $existingUser2 = User::factory()->create(['username' => $existingUsername . '1']);
-
-    //     $data = [
-    //         'name' => $this->faker->name,
-    //         'email' => $existingUser1->email,
-    //         'password' => $this->faker->password,
-    //         'password_confirmation' => $this->faker->password,
-    //     ];
-
-    //     $response = $this->postJson('/api/register', $data);
-
-    //     $response->assertStatus(200)
-    //              ->assertJson(['message' => 'User registered successfully. Please check your email to verify your account.']);
-
-    //     $this->assertDatabaseHas('users', [
-    //         'name' => $data['name'],
-    //         'email' => $data['email'],
-    //         'username' => $existingUsername . '2',
-    //     ]);
-    // }
+        // Check that the user was created with the correct username
+        $this->assertDatabaseHas('users', [
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'username' => $existingUsername . '1',
+        ]);
+    }
 }
