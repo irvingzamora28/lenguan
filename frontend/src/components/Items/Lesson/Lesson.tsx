803c7e5626205f4a6df55c30e53db823ecba5829
Irving Zamora <irvingzamora28@gmail.com>
2023-05-10 00:21:06 -0600

    Display vocabulary words from mdx in the LessonPage component Pass mdx content to Lesson component Put logic to get mdx content and frontmatter in LessonPage component

--- frontend/src/components/Items/Lesson/Lesson.tsx
+++ frontend/src/components/Items/Lesson/Lesson.tsx
@@ -1,95 +1,50 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Markdown from "markdown-to-jsx";
-import { parseFrontmatter } from "../../../utils/parseFrontmatter";
-import { parseContent } from "../../../utils/parseContent";
-
 interface LessonProps {
-	language: string;
-	lessonNumber: number;
+	content: string;
 }
 
-const Lesson: React.FC<LessonProps> = ({ language, lessonNumber }) => {
-	const [lessonData, setLessonData] = useState({ content: "" });
-	const [error, setError] = useState<string | null>(null);
-
-	useEffect(() => {
-		const fetchFileContent = async () => {
-			try {
-				const lessonModule = await import(`../../../lessons/${language}/lesson${lessonNumber}.mdx`);
-				const response = await fetch(lessonModule.default);
-				if (response.ok) {
-					const text = await response.text();
-					// Extract metadata from the MDX content
-					const metadata = parseFrontmatter(text);
-
-					// Do something with the metadata
-					console.log("Metadata:", metadata);
-
-					// Extract the content without the frontmatter
-					const content = parseContent(text);
-					console.log(content);
-
-					setLessonData({ content });
-
-					setError(null);
-				} else {
-					setError(`There was an error getting the lesson content. Please try again later`);
-				}
-			} catch (error) {
-				const errMsg = (error as Error).message;
-				setError(`There was an error getting the lesson content. Please try again later`);
-			}
-		};
-
-		fetchFileContent();
-	}, [language, lessonNumber]);
-
+const Lesson: React.FC<LessonProps> = ({ content }) => {
 	return (
 		<div>
-			{error ? (
-				<div className="error-message">
-					<p>{error}</p>
-				</div>
-			) : (
-				<Markdown
-					options={{
-						overrides: {
-							h1: {
-								props: {
-									className: "py-3 text-2xl md:text-3xl lg:text-4xl",
-								},
-							},
-							h2: {
-								props: {
-									className: "py-3 text-xl md:text-2xl lg:text-3xl",
-								},
-							},
-							ul: {
-								props: {
-									className: "pl-5 py-3 list-disc",
-								},
-							},
-							ol: {
-								props: {
-									className: "pl-5 py-3 list-decimal",
-								},
-							},
-							p: {
-								props: {
-									className: "py-3",
-								},
-							},
-							blockquote: {
-								props: {
-									className: "text-md italic font-normal text-gray-500 dark:text-white",
-								},
+			<Markdown
+				options={{
+					overrides: {
+						h1: {
+							props: {
+								className: "py-3 text-2xl md:text-3xl lg:text-4xl",
 							},
 						},
-					}}
-				>
-					{lessonData.content}
-				</Markdown>
-			)}
+						h2: {
+							props: {
+								className: "py-3 text-xl md:text-2xl lg:text-3xl",
+							},
+						},
+						ul: {
+							props: {
+								className: "pl-5 py-3 list-disc",
+							},
+						},
+						ol: {
+							props: {
+								className: "pl-5 py-3 list-decimal",
+							},
+						},
+						p: {
+							props: {
+								className: "py-3",
+							},
+						},
+						blockquote: {
+							props: {
+								className: "text-md italic font-normal text-gray-500 dark:text-white",
+							},
+						},
+					},
+				}}
+			>
+				{content}
+			</Markdown>
 		</div>
 	);
 };
--- frontend/src/components/Pages/LessonsPage/LessonPage.tsx
+++ frontend/src/components/Pages/LessonsPage/LessonPage.tsx
@@ -1,23 +1,45 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import Layout from "../../Layout/Layout";
-import { AiFillBell } from "react-icons/ai";
+import { parseFrontmatter } from "../../../utils/parseFrontmatter";
+import { parseContent } from "../../../utils/parseContent";
 import { MdOutlineQuiz, MdOutlineTipsAndUpdates } from "react-icons/md";
 import { TbBarbell, TbVocabulary } from "react-icons/tb";
-import Markdown from 'markdown-to-jsx';
+import Layout from "../../Layout/Layout";
 import Lesson from "../../Items/Lesson/Lesson";
-
-interface LayoutProps {
-	children: React.ReactNode;
-}
-
-// surprise, it's a div instead!
-const MyParagraph: React.FC<LayoutProps> = ({ children, ...props }) => (
-    <div {...props}>{children}</div>
-);
 
 const IndividualLessonPage: React.FC = () => {
 	const { id } = useParams<{ id: string }>();
+	const [lessonContent, setLessonContent] = useState("");
+	const [vocabulary, setVocabulary] = useState<string[]>([]);
+	const [error, setError] = useState<string | null>(null);
+
+	useEffect(() => {
+		const fetchFileContent = async () => {
+			try {
+				const lessonModule = await import(`../../../lessons/german/lesson${id}.mdx`);
+				const response = await fetch(lessonModule.default);
+				if (response.ok) {
+					const text = await response.text();
+					// Extract metadata from the MDX content
+					const metadata = parseFrontmatter(text);
+
+					// Extract the content without the frontmatter
+					const content = parseContent(text);
+
+					setLessonContent(content);
+					setVocabulary(metadata?.vocabulary || []);
+					setError(null);
+				} else {
+					setError(`There was an error getting the lesson content. Please try again later`);
+				}
+			} catch (error) {
+				const errMsg = (error as Error).message;
+				setError(`There was an error getting the lesson content. Please try again later`);
+			}
+		};
+
+		fetchFileContent();
+	}, [id]);
 
 	const activities = [
 		{ title: "Exercises", icon: <TbBarbell /> },
@@ -28,15 +50,12 @@ const IndividualLessonPage: React.FC = () => {
 
 	return (
 		<Layout>
-            <AiFillBell />
 			<h2 className="text-2xl font-bold mb-6">Lesson {id}</h2>
 			<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
 				<div className="grid md:grid-flow-col grid-cols-2 md:grid-cols-4 col-span-4 lg:col-span-3 bg-white gap-4 mb-8">
 					{activities.map((activity, index) => (
 						<div key={index} className="bg-white shadow-md rounded-lg p-4 text-center">
-                            <div className="flex justify-center text-3xl">
-							{activity.icon}
-                            </div>
+							<div className="flex justify-center text-3xl">{activity.icon}</div>
 							<h3 className="text-xl font-bold">{activity.title}</h3>
 						</div>
 					))}
@@ -45,15 +64,23 @@ const IndividualLessonPage: React.FC = () => {
 
 			<div className="grid grid-cols-1 md:grid-cols-4 gap-4">
 				<div className="md:col-span-3 bg-white shadow-md rounded-lg p-4">
-					<h3 className="text-xl font-bold mb-4">Lesson Content</h3>
-                    <Lesson language="german" lessonNumber={1} />
+					{error ? (
+						<>
+							<h3 className="text-xl font-bold mb-4">Lesson Content</h3>
+							<div className="error-message">
+								<p>{error}</p>
+							</div>
+						</>
+					) : (
+						<Lesson content={lessonContent} />
+					)}
 				</div>
 				<div className="bg-white shadow-md rounded-lg p-4">
 					<h3 className="text-xl font-bold mb-4">Vocabulary</h3>
 					<ul>
-						<li>Word 1</li>
-						<li>Word 2</li>
-						<li>Word 3</li>
+						{vocabulary?.map((word, index) => (
+							<li key={index}>{word}</li>
+						))}
 					</ul>
 				</div>
 			</div>
