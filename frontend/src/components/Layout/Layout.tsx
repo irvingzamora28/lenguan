2c0721b6a9663a853e3519c239e4fc05c05c2639
Irving Zamora <irvingzamora28@gmail.com>
2023-05-10 22:16:52 -0600

    Store the value in localStorage of asideOpen to persist it and display or hide the sidebarMenu component

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -1,25 +1,38 @@
-// src/components/Layout/Layout.tsx
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../Items/Navbar/Navbar";
 import SidebarMenu from "../Items/Menu/SidebarMenu";
 
 interface LayoutProps {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-	const [profileOpen, setProfileOpen] = useState(false);
-	const [asideOpen, setAsideOpen] = useState(true);
+  const [profileOpen, setProfileOpen] = useState(false);
+  const [asideOpen, setAsideOpen] = useState(() => {
+    const storedAsideOpen = localStorage.getItem("asideOpen");
+    return storedAsideOpen ? JSON.parse(storedAsideOpen) : true;
+  });
 
-	return (
-		<main className="min-h-screen w-full bg-gray-100 text-gray-700">
-			<Navbar asideOpen={asideOpen} setAsideOpen={setAsideOpen} profileOpen={profileOpen} setProfileOpen={setProfileOpen} />
-			<div className="flex">
-				{asideOpen && <SidebarMenu />}
-				<div className="container mx-auto px-4 py-8 max-h-screen md:max-h-none overflow-y-auto">{children}</div>
-			</div>
-		</main>
-	);
+  useEffect(() => {
+    localStorage.setItem("asideOpen", JSON.stringify(asideOpen));
+  }, [asideOpen]);
+
+  return (
+    <main className="min-h-screen w-full bg-gray-100 text-gray-700">
+      <Navbar
+        asideOpen={asideOpen}
+        setAsideOpen={setAsideOpen}
+        profileOpen={profileOpen}
+        setProfileOpen={setProfileOpen}
+      />
+      <div className="flex">
+        {asideOpen && <SidebarMenu />}
+        <div className="container mx-auto px-4 py-8 max-h-screen md:max-h-none overflow-y-auto">
+          {children}
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Layout;
