73570b819fc765d43d5e581119422c5d6289052e
Irving Zamora <irvingzamora28@gmail.com>
2023-05-20 03:03:44 -0600

    Add mp3 play support to the TextToSpeechPlayer component

--- frontend/src/components/Items/Lesson/TextToSpeechPlayer.tsx
+++ frontend/src/components/Items/Lesson/TextToSpeechPlayer.tsx
@@ -3,9 +3,10 @@ import { FiPauseCircle, FiPlayCircle, FiRepeat, FiRewind } from "react-icons/fi"
 
 interface TextToSpeechPlayerProps {
 	text: string;
+	mp3File?: string;
 }
 
-const TextToSpeechPlayer: React.FC<TextToSpeechPlayerProps> = ({ text }) => {
+const TextToSpeechPlayer: React.FC<TextToSpeechPlayerProps> = ({ text, mp3File }) => {
 	const [isPlaying, setIsPlaying] = useState(false);
 	const [isPaused, setIsPaused] = useState(false);
 	const [isRepeating, setIsRepeating] = useState(false);
@@ -14,34 +15,53 @@ const TextToSpeechPlayer: React.FC<TextToSpeechPlayerProps> = ({ text }) => {
 	const utteranceRef = useRef(new SpeechSynthesisUtterance(text));
 	utteranceRef.current.lang = "de-DE";
 
+	const audioRef = useRef<HTMLAudioElement | null>(null);
+
 	useEffect(() => {
 		utteranceRef.current.text = text;
-	}, [text]);
+
+		if (mp3File?.endsWith(".mp3")) {
+			audioRef.current = new Audio(mp3File);
+		}
+	}, [text, mp3File]);
 
 	const handlePlay = () => {
-		utteranceRef.current.rate = 0.8;
-		utteranceRef.current.pitch = 1;
-		window.speechSynthesis.cancel();
-		setIsPlaying(true);
+		if (audioRef.current) {
+			audioRef.current.play();
+		} else {
+			// The rest of your handlePlay function...
+			utteranceRef.current.rate = 0.8;
+			utteranceRef.current.pitch = 1;
+			window.speechSynthesis.cancel();
+			setIsPlaying(true);
 
-		const voices = window.speechSynthesis.getVoices();
-		const selectedVoice = voices.find((voice) => voice.lang === "de-DE");
-		if (selectedVoice) {
-			utteranceRef.current.voice = selectedVoice;
+			const voices = window.speechSynthesis.getVoices();
+			const selectedVoice = voices.find((voice) => voice.lang === "de-DE");
+			if (selectedVoice) {
+				utteranceRef.current.voice = selectedVoice;
+			}
+			window.speechSynthesis.speak(utteranceRef.current);
 		}
-		window.speechSynthesis.speak(utteranceRef.current);
 	};
 
 	const handlePause = () => {
-		setIsPaused(true);
-		window.speechSynthesis.pause();
-		setIsPlaying(false);
+		if (audioRef.current) {
+			audioRef.current.pause();
+		} else {
+			setIsPaused(true);
+			window.speechSynthesis.pause();
+			setIsPlaying(false);
+		}
 	};
 
 	const handleResume = () => {
-		setIsPaused(false);
-		window.speechSynthesis.resume();
-		setIsPlaying(true);
+		if (audioRef.current) {
+			audioRef.current.play();
+		} else {
+			setIsPaused(false);
+			window.speechSynthesis.resume();
+			setIsPlaying(true);
+		}
 	};
 
 	const handlePlayPause = () => {
@@ -55,10 +75,15 @@ const TextToSpeechPlayer: React.FC<TextToSpeechPlayerProps> = ({ text }) => {
 	};
 
 	const handleRestart = () => {
-		window.speechSynthesis.cancel();
-		setIsPlaying(false);
-		setIsPaused(false);
-		setIsRestarting(true);
+		if (audioRef.current) {
+			audioRef.current.currentTime = 0;
+			audioRef.current.play();
+		} else {
+			window.speechSynthesis.cancel();
+			setIsPlaying(false);
+			setIsPaused(false);
+			setIsRestarting(true);
+		}
 	};
 
 	useEffect(() => {
@@ -76,17 +101,20 @@ const TextToSpeechPlayer: React.FC<TextToSpeechPlayerProps> = ({ text }) => {
 	}, []);
 
 	return (
-		<div className="bg-green-200 rounded-lg p-4 flex items-center justify-center space-x-4 shadow-lg hover:shadow-xl">
-			<button onClick={handleRestart} className="hover:bg-green-300 hover:text-white rounded-full p-2">
-				<FiRewind size={24} />
-			</button>
-			<button onClick={handlePlayPause} className="hover:bg-green-300 hover:text-white rounded-full p-2">
-				{isPlaying ? <FiPauseCircle size={24} /> : <FiPlayCircle size={24} />}
-			</button>
-			<button onClick={handleRepeat} className="hover:bg-green-300 hover:text-white rounded-full p-2">
-				<FiRepeat size={24} className={`text-green-600 ${isRepeating && "text-white bg-green-600 rounded-full"}`} />
-			</button>
-		</div>
+		<>
+			<p className={`bg-primary-200 dark:bg-primary-800`}>{text}</p>
+			<div className="bg-green-200 rounded-lg p-4 flex items-center justify-center space-x-4 shadow-lg hover:shadow-xl">
+				<button onClick={handleRestart} className="hover:bg-green-300 hover:text-white rounded-full p-2">
+					<FiRewind size={24} />
+				</button>
+				<button onClick={handlePlayPause} className="hover:bg-green-300 hover:text-white rounded-full p-2">
+					{isPlaying ? <FiPauseCircle size={24} /> : <FiPlayCircle size={24} />}
+				</button>
+				<button onClick={handleRepeat} className="hover:bg-green-300 hover:text-white rounded-full p-2">
+					<FiRepeat size={24} className={`text-green-600 ${isRepeating && "text-white bg-green-600 rounded-full"}`} />
+				</button>
+			</div>
+		</>
 	);
 };
 
