61eec0e42e859802dd73d7c1ed781344011790ef
Irving Zamora <irvingzamora28@gmail.com>
2023-05-20 00:49:35 -0600

    feat: Enhance card component for flexible content alignment
    This commit updates the card component to use flexbox for more flexible content alignment. It allows the ul element to align to the bottom of each card, regardless of the height of the card. The change improves visual consistency across cards of varying heights.
    
    - Add flex and flex-col classes to card container
    - Add flex-grow to the content container to fill remaining space
    - Ensure the card fills its container's height using h-full class

--- frontend/src/components/Items/Cards/LessonCard.tsx
+++ frontend/src/components/Items/Cards/LessonCard.tsx
@@ -1,40 +1,41 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 interface LessonCardProps {
-  _id: string;
-  image: string;
-  name: string;
-  description: string;
-  progress: number;
-  goals: string[];
+	_id: string;
+	image: string;
+	name: string;
+	description: string;
+	progress: number;
+	goals: string[];
 }
 
 const LessonCard: React.FC<LessonCardProps> = ({ _id, image, name, description, progress, goals }) => {
-  return (
-    <div className="rounded-lg shadow-xl bg-white">
-      <img src={image} alt={name} className="w-full h-40 rounded-t-lg object-cover" />
-      <div className="p-4">
-        <Link to={`/lessons/${_id}`} className="text-decoration-none">
-          <h3 className="text-xl font-bold mb-2">{name}</h3>
-        </Link>
-        <p className="text-gray-700 mb-4">{description}</p>
-        <div className="w-full h-2 bg-gray-200 rounded mb-4">
-          <div
-            className="h-2 bg-blue-500 rounded"
-            style={{ width: `${progress}%` }}
-          ></div>
-        </div>
-        <ul className="flex flex-wrap">
-          {goals.map((goal, index) => (
-            <li key={index} className="text-sm bg-gray-200 px-2 py-1 rounded mr-1 mb-1">
-              {goal}
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex flex-col h-full rounded-lg shadow-xl bg-white">
+			<img src={image} alt={name} className="w-full h-40 rounded-t-lg object-cover" />
+			<div className="flex flex-col justify-between flex-grow p-4">
+				<div>
+					<Link to={`/lessons/${_id}`} className="text-decoration-none">
+						<h3 className="text-xl font-bold mb-2">{name}</h3>
+					</Link>
+					<p className="text-gray-700 mb-4">{description}</p>
+				</div>
+				<div className="flex flex-wrap">
+					<div className="w-full h-2 bg-gray-200 rounded mb-4">
+						<div className="h-2 bg-blue-500 rounded" style={{ width: `${progress}%` }}></div>
+					</div>
+					<ul className="flex flex-wrap">
+						{goals.map((goal, index) => (
+							<li key={index} className="text-sm bg-gray-200 px-2 py-1 rounded mr-1 mb-1">
+								{goal}
+							</li>
+						))}
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default LessonCard;
