4ff8f8c714469a3383812e478fe3998c137e745e
Irving Zamora <irvingzamora28@gmail.com>
2023-05-20 01:15:53 -0600

    Display NotFoundPage component if the lesson content is not found

--- frontend/src/components/Pages/LessonsPage/LessonPage.tsx
+++ frontend/src/components/Pages/LessonsPage/LessonPage.tsx
@@ -7,6 +7,7 @@ import { TbBarbell, TbVocabulary } from "react-icons/tb";
 import Layout from "../../Layout/Layout";
 import Lesson from "../../Items/Lesson/Lesson";
 import TextToSpeechPlayer from "../../Items/Lesson/TextToSpeechPlayer";
+import NotFoundPage from "../NotFoundPage";
 
 const IndividualLessonPage: React.FC = () => {
 	const { id } = useParams<{ id: string }>();
@@ -51,41 +52,38 @@ const IndividualLessonPage: React.FC = () => {
 
 	return (
 		<Layout>
-			<h2 className="text-2xl font-bold mb-6">Lesson {id}</h2>
-			<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
-				<div className="grid md:grid-flow-col grid-cols-2 md:grid-cols-4 col-span-4 lg:col-span-3 bg-white gap-4 mb-8">
-					{activities.map((activity, index) => (
-						<div key={index} className="bg-white shadow-md rounded-lg p-4 text-center">
-							<div className="flex justify-center text-3xl">{activity.icon}</div>
-							<h3 className="text-xl font-bold">{activity.title}</h3>
+			{error ? (
+				<NotFoundPage />
+			) : (
+				<>
+					<h2 className="text-2xl font-bold mb-6">Lesson {id}</h2>
+					<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
+						<div className="grid md:grid-flow-col grid-cols-2 md:grid-cols-4 col-span-4 lg:col-span-3 bg-white gap-4 mb-8">
+							{activities.map((activity, index) => (
+								<div key={index} className="bg-white shadow-md rounded-lg p-4 text-center">
+									<div className="flex justify-center text-3xl">{activity.icon}</div>
+									<h3 className="text-xl font-bold">{activity.title}</h3>
+								</div>
+							))}
 						</div>
-					))}
-				</div>
-			</div>
+					</div>
 
-			<div className="grid grid-cols-1 md:grid-cols-4 gap-4">
-				<div className="md:col-span-3 bg-white shadow-md rounded-lg p-4">
-					<TextToSpeechPlayer text="Hallo, wie geht es dir?" />
-					{error ? (
-						<>
-							<h3 className="text-xl font-bold mb-4">Lesson Content</h3>
-							<div className="error-message">
-								<p>{error}</p>
-							</div>
-						</>
-					) : (
-						<Lesson content={lessonContent} />
-					)}
-				</div>
-				<div className="bg-white shadow-md rounded-lg p-4">
-					<h3 className="text-xl font-bold mb-4">Vocabulary</h3>
-					<ul>
-						{vocabulary?.map((word, index) => (
-							<li key={index}>{word}</li>
-						))}
-					</ul>
-				</div>
-			</div>
+					<div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+						<div className="md:col-span-3 bg-white shadow-md rounded-lg p-4">
+							<TextToSpeechPlayer text="Hallo, wie geht es dir?" />
+							<Lesson content={lessonContent} />
+						</div>
+						<div className="bg-white shadow-md rounded-lg p-4">
+							<h3 className="text-xl font-bold mb-4">Vocabulary</h3>
+							<ul>
+								{vocabulary?.map((word, index) => (
+									<li key={index}>{word}</li>
+								))}
+							</ul>
+						</div>
+					</div>
+				</>
+			)}
 		</Layout>
 	);
 };
--- frontend/src/components/Pages/NotFoundPage.tsx
+++ frontend/src/components/Pages/NotFoundPage.tsx
@@ -1,41 +1,48 @@
 import { isRouteErrorResponse, useRouteError } from "react-router-dom";
 
 const NotFoundPage = () => {
+	const error = useRouteError();
 
-  const error = useRouteError();
+	if (isRouteErrorResponse(error)) {
+		if (error.status === 401) {
+			// ...
+		} else if (error.status === 404) {
+			// ...
+		}
 
-  if (isRouteErrorResponse(error)) {
-    if (error.status === 401) {
-      // ...
-    }
-    else if (error.status === 404) {
-      // ...
-    }
-
-    return (
-      <div id="error-page">
-        <h1>Oops! {error.status}</h1>
-        <p>{error.statusText}</p>
-        {error.data?.message && (
-          <p>
-            <i>{error.data.message}</i>
-          </p>
-        )}
-      </div>
-    );
-  } else if (error instanceof Error) {
-    return (
-      <div id="error-page">
-        <h1>Oops! Unexpected Error</h1>
-        <p>Something went wrong.</p>
-        <p>
-          <i>{error.message}</i>
-        </p>
-      </div>
-    );
-  } else {
-    return <></>;
-  }
-}
+		return (
+			<div id="error-page">
+				<h1>Oops! {error.status}</h1>
+				<p>{error.statusText}</p>
+				{error.data?.message && (
+					<p>
+						<i>{error.data.message}</i>
+					</p>
+				)}
+			</div>
+		);
+	} else if (error instanceof Error) {
+		return (
+			<div id="error-page">
+				<h1>Oops! Unexpected Error</h1>
+				<p>Something went wrong.</p>
+				<p>
+					<i>{error.message}</i>
+				</p>
+			</div>
+		);
+	} else {
+		return (
+			<div className="flex flex-col items-center justify-center h-screen text-gray-600">
+				<h1 className="text-6xl font-semibold">404</h1>
+				<p className="text-2xl">Oops! Page not found.</p>
+				<p className="mt-2">We could not find the page you were looking for.</p>
+				<a href="/" className="mt-6 text-blue-600 hover:text-blue-800 hover:underline">
+					Go back home
+				</a>
+			</div>
+		);
+	}
+};
 
 export default NotFoundPage;
