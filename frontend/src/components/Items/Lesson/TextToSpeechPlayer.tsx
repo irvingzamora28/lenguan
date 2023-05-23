c2113d4703625eeeb3e1972356458eef305f71b8
Irving Zamora <irvingzamora28@gmail.com>
2023-05-22 20:10:23 -0600

    Add a progress bar to the TextToSpeechPlayer component

--- frontend/src/components/Items/Lesson/TextToSpeechPlayer.tsx
+++ frontend/src/components/Items/Lesson/TextToSpeechPlayer.tsx
@@ -12,11 +12,30 @@ const TextToSpeechPlayer: React.FC<TextToSpeechPlayerProps> = ({ text, displayTe
 	const [isPaused, setIsPaused] = useState(false);
 	const [isRepeating, setIsRepeating] = useState(false);
 	const [isRestarting, setIsRestarting] = useState(false);
+	const [currentTime, setCurrentTime] = useState(0);
+	const [duration, setDuration] = useState(0);
 
 	const utteranceRef = useRef(new SpeechSynthesisUtterance(text));
 	utteranceRef.current.lang = "de-DE";
 
 	const audioRef = useRef<HTMLAudioElement | null>(null);
+
+	// Listen to the time update event and update the currentTime state
+	useEffect(() => {
+		if (audioRef.current) {
+			audioRef.current.addEventListener("timeupdate", () => {
+				if (audioRef.current) {
+					setCurrentTime(audioRef.current.currentTime);
+				}
+			});
+
+			audioRef.current.addEventListener("loadedmetadata", function () {
+				if (audioRef.current) {
+					setDuration(audioRef.current.duration);
+				}
+			});
+		}
+	}, [audioRef.current]);
 
 	useEffect(() => {
 		utteranceRef.current.text = text;
@@ -26,51 +45,75 @@ const TextToSpeechPlayer: React.FC<TextToSpeechPlayerProps> = ({ text, displayTe
 		}
 	}, [text, mp3File]);
 
+	useEffect(() => {
+		if (audioRef.current) {
+			const handleEnd = () => {
+				if (isRepeating) {
+					handleRestart();
+				}
+			};
+
+			// Add or remove the event listener based on isRepeating
+			if (isRepeating) {
+				audioRef.current.addEventListener("ended", handleEnd);
+			} else {
+				audioRef.current.removeEventListener("ended", handleEnd);
+			}
+		}
+	}, [isRepeating]);
+
+	const handleProgressBarClick = (event: any) => {
+		const newTime = event.target.value;
+		setCurrentTime(newTime);
+		if (audioRef.current) {
+			audioRef.current.currentTime = newTime;
+		}
+	};
+
 	const handlePlay = () => {
-        if (audioRef.current) {
-            audioRef.current.play();
-            setIsPlaying(true);
-            setIsPaused(false);
-        } else {
-            utteranceRef.current.rate = 0.8;
-            utteranceRef.current.pitch = 1;
-            window.speechSynthesis.cancel();
-            setIsPlaying(true);
-            setIsPaused(false);
+		if (audioRef.current) {
+			audioRef.current.play();
+			setIsPlaying(true);
+			setIsPaused(false);
+		} else {
+			utteranceRef.current.rate = 0.8;
+			utteranceRef.current.pitch = 1;
+			window.speechSynthesis.cancel();
+			setIsPlaying(true);
+			setIsPaused(false);
 
-            const voices = window.speechSynthesis.getVoices();
-            const selectedVoice = voices.find((voice) => voice.lang === "de-DE");
-            if (selectedVoice) {
-                utteranceRef.current.voice = selectedVoice;
-            }
-            window.speechSynthesis.speak(utteranceRef.current);
-        }
-    };
+			const voices = window.speechSynthesis.getVoices();
+			const selectedVoice = voices.find((voice) => voice.lang === "de-DE");
+			if (selectedVoice) {
+				utteranceRef.current.voice = selectedVoice;
+			}
+			window.speechSynthesis.speak(utteranceRef.current);
+		}
+	};
 
-    const handlePause = () => {
-        if (audioRef.current) {
-            audioRef.current.pause();
-            setIsPlaying(false);
-            setIsPaused(true);
-        } else {
-            setIsPaused(true);
-            setIsPlaying(false);
-            window.speechSynthesis.pause();
-        }
-    };
+	const handlePause = () => {
+		if (audioRef.current) {
+			audioRef.current.pause();
+			setIsPlaying(false);
+			setIsPaused(true);
+		} else {
+			setIsPaused(true);
+			setIsPlaying(false);
+			window.speechSynthesis.pause();
+		}
+	};
 
-    const handleResume = () => {
-        if (audioRef.current) {
-            audioRef.current.play();
-            setIsPlaying(true);
-            setIsPaused(false);
-        } else {
-            setIsPaused(false);
-            setIsPlaying(true);
-            window.speechSynthesis.resume();
-        }
-    };
-
+	const handleResume = () => {
+		if (audioRef.current) {
+			audioRef.current.play();
+			setIsPlaying(true);
+			setIsPaused(false);
+		} else {
+			setIsPaused(false);
+			setIsPlaying(true);
+			window.speechSynthesis.resume();
+		}
+	};
 
 	const handlePlayPause = () => {
 		if (!isPlaying && !isPaused) handlePlay();
@@ -119,8 +162,9 @@ const TextToSpeechPlayer: React.FC<TextToSpeechPlayerProps> = ({ text, displayTe
 					{isPlaying ? <FiPauseCircle size={24} /> : <FiPlayCircle size={24} />}
 				</button>
 				<button onClick={handleRepeat} className="hover:bg-green-300 hover:text-white rounded-full p-2">
-					<FiRepeat size={24} className={`text-green-600 ${isRepeating && "text-white bg-green-600 rounded-full"}`} />
+					<FiRepeat size={24} className={`${isRepeating && "text-green-600 rounded-full"}`} />
 				</button>
+				{mp3File && <input type="range" min={0} max={duration} value={currentTime} onChange={handleProgressBarClick} className="w-full slider accent-slate-600" />}
 			</div>
 		</>
 	);
