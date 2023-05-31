c69df59cc2d1fc446a6ead026d8146b3fb0a8cde
Irving Zamora <irvingzamora28@gmail.com>
2023-05-30 20:13:08 -0600

    Center Start button from ListeningExercise

--- frontend/src/components/Pages/LessonsPage/ListeningExercise.tsx
+++ frontend/src/components/Pages/LessonsPage/ListeningExercise.tsx
@@ -76,7 +76,7 @@ const ListeningExercise: React.FC = () => {
 	const [soundEffect, setSoundEffect] = useState<string | null>(null);
 	const [selectedAnswer, setSelectedAnswer] = useState<string>("");
 	const [progress, setProgress] = useState<number>(0);
-    const [gameStarted, setGameStarted] = useState<boolean>(false);
+	const [gameStarted, setGameStarted] = useState<boolean>(false);
 
 	const checkAnswer = (selectedAnswer: string) => {
 		setSelectedAnswer(selectedAnswer);
@@ -108,58 +108,64 @@ const ListeningExercise: React.FC = () => {
 	}, []);
 
 	useEffect(() => {
-        if (gameStarted && currentExercise[exerciseIndex]) {
-            const audioElement = document.getElementById('exercise-audio') as HTMLAudioElement;
-            setTimeout(() => {
-                audioElement.load();
-                audioElement.play();
-            }, 1000); // wait for 1 second before playing the next audio
-        }
-    }, [exerciseIndex, gameStarted, currentExercise]);
+		if (gameStarted && currentExercise[exerciseIndex]) {
+			const audioElement = document.getElementById("exercise-audio") as HTMLAudioElement;
+			setTimeout(() => {
+				audioElement.load();
+				audioElement.play();
+			}, 1000); // wait for 1 second before playing the next audio
+		}
+	}, [exerciseIndex, gameStarted, currentExercise]);
 
 	// TODO: Add ability to set time limit
 	return (
-            <Layout>
+		<Layout>
 			{!gameStarted ? (
-				<button
-					className="flex items-center shadow-box justify-center h-24 w-64 drop-shadow-xl rounded-lg px-8 py-4 overflow-hidden group bg-yellow-400 relative hover:bg-gradient-to-r hover:from-yellow-400 hover:to-yellow-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-yellow-400 transition-all ease-out duration-300"
-					onClick={() => setGameStarted(true)}
-				>
-					<span className="absolute right-0 w-12 h-44 -mt-12 transition-all duration-1000 transform translate-x-16 bg-white opacity-10 rotate-12 group-hover:-translate-x-72 ease"></span>
-					<span className="relative gender_duel__text-shadow text-4xl font-bold">START</span>
-				</button>
+				<div className="flex">
+					<button
+						className="items-center mx-auto shadow-box justify-center h-24 w-64 drop-shadow-xl rounded-lg px-8 py-4 overflow-hidden group bg-yellow-400 relative hover:bg-gradient-to-r hover:from-yellow-400 hover:to-yellow-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-yellow-400 transition-all ease-out duration-300"
+						onClick={() => setGameStarted(true)}
+					>
+						<span className="absolute right-0 w-12 h-44 -mt-12 transition-all duration-1000 transform translate-x-16 bg-white opacity-10 rotate-12 group-hover:-translate-x-72 ease"></span>
+						<span className="relative gender_duel__text-shadow text-4xl font-bold">START</span>
+					</button>
+				</div>
 			) : (
-                <>
-                {!currentExercise[exerciseIndex] ? (
-                    <div>
-                        <h1>Listening Exercise</h1>
-                        <p>No more exercises!</p>
-                    </div>
-                ) : (
-                    <>
-                        <div className="w-full max-w-md mx-auto">
-                            <h1 className="text-2xl font-bold mb-4">Match the Audio</h1>
-                            <audio id="exercise-audio" controls src={currentExercise[exerciseIndex]?.audio}>
-                                Your browser does not support the audio element.
-                            </audio>
-                            <div className="mt-4">
-                                {currentExercise[exerciseIndex].options.map((option, index) => (
-                                    <button key={index} className={`block w-full p-2 mb-2 text-left ${option === selectedAnswer ? "bg-primary-200" : "bg-backgroundalt"} border border-gray-200 rounded`} onClick={() => checkAnswer(option)}>
-                                        {option}
-                                    </button>
-                                ))}
-                            </div>
-                            <div className="mt-4 py-2">
-                                <div className="w-full bg-backgroundalt border border-gray-200 rounded">
-                                    <div className="h-2 bg-primary-600 rounded" style={{ width: `${(progress / currentExercise.length) * 100}%` }} />
-                                </div>
-                                <p className="my-2">
-                                    Progress: {progress} / {currentExercise.length}
-                                </p>
-                            </div>
-                        </div>
-                    </>
-                )}
+				<>
+					{!currentExercise[exerciseIndex] ? (
+						<div>
+							<h1>Listening Exercise</h1>
+							<p>No more exercises!</p>
+						</div>
+					) : (
+						<>
+							<div className="w-full max-w-md mx-auto">
+								<h1 className="text-2xl font-bold mb-4">Match the Audio</h1>
+								<audio id="exercise-audio" controls src={currentExercise[exerciseIndex]?.audio}>
+									Your browser does not support the audio element.
+								</audio>
+								<div className="mt-4">
+									{currentExercise[exerciseIndex].options.map((option, index) => (
+										<button
+											key={index}
+											className={`block w-full p-2 mb-2 text-left ${option === selectedAnswer ? "bg-primary-200" : "bg-backgroundalt"} border border-gray-200 rounded`}
+											onClick={() => checkAnswer(option)}
+										>
+											{option}
+										</button>
+									))}
+								</div>
+								<div className="mt-4 py-2">
+									<div className="w-full bg-backgroundalt border border-gray-200 rounded">
+										<div className="h-2 bg-primary-600 rounded" style={{ width: `${(progress / currentExercise.length) * 100}%` }} />
+									</div>
+									<p className="my-2">
+										Progress: {progress} / {currentExercise.length}
+									</p>
+								</div>
+							</div>
+						</>
+					)}
 					{/* your existing code... */}
 				</>
 			)}
