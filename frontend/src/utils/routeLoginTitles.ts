a58ab5640da3a1f08a33f5de8aaf1b175034db72
Irving Zamora <irvingzamora28@gmail.com>
2023-06-16 19:24:25 -0600

    feat: Add dynamic page title and change login form style
    - Implement dynamic login page title based on the previous route.
    - Improve login page responsiveness with Tailwind CSS classes.

---
+++ frontend/src/utils/routeLoginTitles.ts
@@ -0,0 +1,8 @@
+export const routeTitles: Record<string, string> = {
+	"/": "",
+	"/lessons": "Lessons",
+	"/lessons/:id": "Lesson",
+	"/exercises": "Exercises",
+	"/listening-exercise/:id": "Listening Exercise",
+	"/gender-duel": "Gender Duel",
+};
