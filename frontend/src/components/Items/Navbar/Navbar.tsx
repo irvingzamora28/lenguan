655c04e9e9e9ec550380ad8d0d0a23cc1021cf0f
Irving Zamora <irvingzamora28@gmail.com>
2023-07-14 17:22:12 -0600

    bugfix: Display correctly profile menu in navbar

--- frontend/src/components/Items/Navbar/Navbar.tsx
+++ frontend/src/components/Items/Navbar/Navbar.tsx
@@ -75,36 +75,38 @@ const Navbar: React.FC<NavBarProps> = ({ asideOpen, setAsideOpen, profileOpen, s
 						)}
 					</div>
 				)}
-				<button type="button" onClick={() => setProfileOpen(!profileOpen)} className="h-9 w-9 overflow-hidden rounded-full profile-button" aria-label="profile">
-					<img src="https://picsum.photos/45" alt="plchldr.co" />
-				</button>
-				{profileOpen && (
-					<div ref={profileMenuRef} className="absolute right-2 mt-1 w-48 divide-y divide-gray-200 rounded-md border border-gray-200 bg-backgroundalt shadow-md">
-						<div className="flex items-center space-x-2 p-2">
-							<img src="https://picsum.photos/45" alt="plchldr.co" className="h-9 w-9 rounded-full" />
-							<div className="font-medium">{user?.name}</div>
+				<div className="relative">
+					<button type="button" onClick={() => setProfileOpen(!profileOpen)} className="h-9 w-9 overflow-hidden rounded-full profile-button" aria-label="profile">
+						<img src="https://picsum.photos/45" alt="plchldr.co" />
+					</button>
+					{profileOpen && (
+						<div ref={profileMenuRef} className="absolute right-0 mt-1 w-48 divide-y divide-gray-200 rounded-md border border-gray-200 bg-backgroundalt shadow-md">
+							<div className="flex items-center space-x-2 p-2">
+								<img src="https://picsum.photos/45" alt="plchldr.co" className="h-9 w-9 rounded-full" />
+								<div className="font-medium">{user?.name}</div>
+							</div>
+							<div className="flex flex-col space-y-3 p-2">
+								<a href="#" className="transition hover:text-primary-600">
+									My Profile
+								</a>
+								<a href="#" className="transition hover:text-primary-600">
+									Edit Profile
+								</a>
+								<a href="#" className="transition hover:text-primary-600">
+									Settings
+								</a>
+							</div>
+							<div className="p-2">
+								<button className="flex items-center space-x-2 transition hover:text-primary-600" onClick={handleLogout}>
+									<svg className="h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+										<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 013-3V7a3 3 0 013-3v1"></path>
+									</svg>
+									<div>Log Out</div>
+								</button>
+							</div>
 						</div>
-						<div className="flex flex-col space-y-3 p-2">
-							<a href="#" className="transition hover:text-primary-600">
-								My Profile
-							</a>
-							<a href="#" className="transition hover:text-primary-600">
-								Edit Profile
-							</a>
-							<a href="#" className="transition hover:text-primary-600">
-								Settings
-							</a>
-						</div>
-						<div className="p-2">
-							<button className="flex items-center space-x-2 transition hover:text-primary-600" onClick={handleLogout}>
-								<svg className="h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-									<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 013-3V7a3 3 0 013-3v1"></path>
-								</svg>
-								<div>Log Out</div>
-							</button>
-						</div>
-					</div>
-				)}
+					)}
+				</div>
 			</div>
 		</header>
 	);
