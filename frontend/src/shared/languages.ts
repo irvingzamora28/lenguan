030743342a5e3824bc0b833734fdca37e7ef3324
Irving Zamora <irvingzamora28@gmail.com>
2023-07-17 20:27:01 -0600

    Remove languages array file as it is no longer needed

--- frontend/src/shared/languages.ts
+++
@@ -1,7 +0,0 @@
-export const languages = [
-    { title: "English", code: "en" },
-    { title: "German", code: "de" },
-    { title: "French", code: "fr" },
-    { title: "Spanish", code: "es" },
-    // Add more languages as needed
-  ];
