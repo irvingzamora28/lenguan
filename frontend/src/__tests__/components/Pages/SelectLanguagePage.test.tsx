98a6eb574155e6e2c243a12c92ee3147dbb721b2
Irving Zamora <irvingzamora28@gmail.com>
2023-10-19 12:03:00 -0600

    test: Mock getLanguages function to fix SelectLanguagePage test

--- frontend/src/__tests__/components/Pages/SelectLanguagePage.test.tsx
+++ frontend/src/__tests__/components/Pages/SelectLanguagePage.test.tsx
@@ -1,13 +1,32 @@
-import React from "react";
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { render, fireEvent, waitFor } from "@testing-library/react";
 import { Provider } from "react-redux";
 import store from "../../../redux/store";
 import SelectLanguagePage from "../../../components/Pages/SelectLanguagePage";
 import { BrowserRouter } from "react-router-dom";
+import { vi } from "vitest";
+import { setLanguages } from "../../../redux/languageSlice";
+
+vi.mock("../../utils/languages", () => {
+	return {
+		getLanguages: (languages: any, dispatch: any) => {
+			dispatch(setLanguages(mockLanguageData));
+		},
+	};
+});
+
+const mockLanguageData = [
+	{ _id: "1", name: "English", code: "en" },
+	{ _id: "2", name: "Spanish", code: "es" },
+	{ _id: "3", name: "French", code: "fr" },
+];
 
 describe("SelectLanguagePage", () => {
+	beforeEach(() => {
+		vi.clearAllMocks();
+	});
+
 	it("selects language on click", async () => {
-		const { getByLabelText, getByText } = render(
+		const { getByText } = render(
 			<Provider store={store}>
 				<BrowserRouter>
 					<SelectLanguagePage />
@@ -15,7 +34,9 @@ describe("SelectLanguagePage", () => {
 			</Provider>
 		);
 
-		const languageBox = getByText("French"); // Assuming 'French' is one of the languages in `languages`
+		await waitFor(() => expect(getByText("French")).toBeInTheDocument());
+
+		const languageBox = getByText("French");
 		fireEvent.click(languageBox);
 
 		await waitFor(() => {
