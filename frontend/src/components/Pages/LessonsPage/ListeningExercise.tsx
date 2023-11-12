134b72e8a09d03c33dc0085a4e62342444cd6706
Irving Zamora <irvingzamora28@gmail.com>
2023-11-11 21:19:21 -0600

    refactor(ListeningExercise): Improve State Handling in ListeningExercise Component - Separated game state and user interaction state for clearer state management.

--- frontend/src/components/Pages/LessonsPage/ListeningExercise.tsx
+++ frontend/src/components/Pages/LessonsPage/ListeningExercise.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import correctSound from "../../../assets/audio/correct-choice.mp3";
 import incorrectSound from "../../../assets/audio/incorrect-choice.mp3";
 import Layout from "../../Layout/Layout";
@@ -72,42 +72,52 @@ const shuffleArray = (array: any[]) => {
 };
 
 const ListeningExercise: React.FC = () => {
-	const [exerciseIndex, setExerciseIndex] = useState(0);
-	const [currentExercise, setCurrentExercise] = useState<Exercise[]>([]);
-	const [soundEffect, setSoundEffect] = useState<string | null>(null);
-	const [selectedAnswer, setSelectedAnswer] = useState<string>("");
-	const [progress, setProgress] = useState<number>(0);
-	const [gameStarted, setGameStarted] = useState<boolean>(false);
 
-	const checkAnswer = (selectedAnswer: string) => {
-		setSelectedAnswer(selectedAnswer);
+	const [state, setState] = useState({
+		exerciseIndex: 0,
+		currentExercise: [] as Exercise[],
+		progress: 0,
+		gameStarted: false,
+	});
 
-		if (currentExercise[exerciseIndex] && selectedAnswer === currentExercise[exerciseIndex].correctAnswer) {
-			setSoundEffect(correctSound);
-            setTimeout(() => {
-                setExerciseIndex((prevIndex) => prevIndex + 1);
-                setSelectedAnswer("");
-                setProgress((prevProgress) => prevProgress + 1);
-			}, 1000); // wait for 1 second before playing the next audio
-		} else {
-			setSoundEffect(incorrectSound);
-		}
+    const playSound = (soundEffect: string) => {
+		const audio = new Audio(soundEffect);
+		audio.play();
 	};
 
-	useEffect(() => {
-		if (soundEffect) {
-			const audio = new Audio(soundEffect);
-			audio.play();
-		}
-		setSoundEffect(null);
-	}, [soundEffect]);
+	const { exerciseIndex, currentExercise, progress, gameStarted } = state;
+	const [selectedAnswer, setSelectedAnswer] = useState("");
+
+	const updateState = (newState: Partial<typeof state>) => setState((prevState) => ({ ...prevState, ...newState }));
+
+	const checkAnswer = useCallback(
+		(answer: string) => {
+			return () => {
+				setSelectedAnswer(answer);
+
+				if (currentExercise[exerciseIndex] && answer === currentExercise[exerciseIndex].correctAnswer) {
+					playSound(correctSound);
+					setTimeout(() => {
+						updateState({
+							exerciseIndex: exerciseIndex + 1,
+							progress: progress + 1,
+						});
+						setSelectedAnswer("");
+					}, 1000);
+				} else {
+					playSound(incorrectSound);
+				}
+			};
+		},
+		[exerciseIndex, currentExercise, progress]
+	);
 
 	useEffect(() => {
 		const shuffledExercises = JSON.parse(JSON.stringify(initialExercises)); // Deep copy
 		shuffledExercises.forEach((exercise: Exercise) => {
 			exercise.options = shuffleArray(exercise.options);
 		});
-		setCurrentExercise(shuffledExercises);
+		updateState({ currentExercise: shuffledExercises });
 	}, []);
 
 	// TODO: Add ability to set time limit
@@ -117,7 +127,7 @@ const ListeningExercise: React.FC = () => {
 				<div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
 					<button
 						className="items-center mx-auto shadow-box justify-center h-24 w-64 drop-shadow-xl rounded-lg px-8 py-4 overflow-hidden group bg-yellow-400 relative hover:bg-gradient-to-r hover:from-yellow-400 hover:to-yellow-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-yellow-400 transition-all ease-out duration-300"
-						onClick={() => setGameStarted(true)}
+						onClick={() => updateState({ gameStarted: true })}
 					>
 						<span className="absolute right-0 w-12 h-44 -mt-12 transition-all duration-1000 transform translate-x-16 bg-white opacity-10 rotate-12 group-hover:-translate-x-72 ease"></span>
 						<span className="relative gender_duel__text-shadow text-4xl font-bold">START</span>
@@ -134,13 +144,13 @@ const ListeningExercise: React.FC = () => {
 						<>
 							<div className="w-full max-w-md mx-auto">
 								<h1 className="text-2xl font-bold mb-4">Match the Audio</h1>
-                                <TextToSpeechPlayer autoplay mp3File={currentExercise[exerciseIndex]?.audio} ></TextToSpeechPlayer>
+                                <TextToSpeechPlayer autoplay mp3File={currentExercise[exerciseIndex]?.audio} />
 								<div className="mt-4">
 									{currentExercise[exerciseIndex].options.map((option, index) => (
 										<button
 											key={index}
 											className={`block w-full p-2 mb-2 text-left ${option === selectedAnswer ? "bg-primary-200" : "bg-backgroundalt"} border border-gray-200 rounded`}
-											onClick={() => checkAnswer(option)}
+											onClick={checkAnswer(option)}
 										>
 											{option}
 										</button>
