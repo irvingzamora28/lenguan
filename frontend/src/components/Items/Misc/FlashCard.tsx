a9984aee681efa53470672befbfc86966dd648c2
Irving Zamora <irvingzamora28@gmail.com>
2023-12-01 01:42:13 -0600

    feat(vocabulary): Add Flip Animation to FlashCard Component - Implement interactive flip effect on flashcard click - Add lesson 7 audio and image vocabulary assets

--- frontend/src/components/Items/Misc/FlashCard.tsx
+++ frontend/src/components/Items/Misc/FlashCard.tsx
@@ -1,36 +1,61 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, useRef } from "react";
 
 type FlashCardProps = {
-  word: string;
-  translation: string;
-  audioUrl: string;
-  imageUrl: string;
+	word: string;
+	translation: string;
+	audioUrl: string;
+	imageUrl: string;
 };
 
 const FlashCard: React.FC<FlashCardProps> = ({ word, translation, audioUrl, imageUrl }) => {
-  const [flipped, setFlipped] = useState(false);
+	const [flipped, setFlipped] = useState(false);
+	const audioRef = useRef<HTMLAudioElement>(null);
 
-  const toggleFlip = () => {
-    setFlipped(!flipped);
-  };
+	const toggleFlip = () => {
+		setFlipped(!flipped);
+	};
 
-  return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg cursor-pointer" onClick={toggleFlip}>
-      <img className="w-full" src={imageUrl} alt={word} />
-      <div className="px-6 py-4">
-        {flipped ? (
-          <>
-            <div className="font-bold text-xl mb-2">{translation}</div>
-            <audio controls src={audioUrl}>
-              Your browser does not support the audio element.
-            </audio>
-          </>
-        ) : (
-          <div className="font-bold text-xl mb-2">{word}</div>
-        )}
-      </div>
-    </div>
-  );
+	useEffect(() => {
+		if (flipped && audioRef.current) {
+			audioRef.current.play();
+		}
+	}, [flipped]);
+
+	return (
+		<div
+			className={`relative max-w-sm mx-auto h-96 shadow-lg rounded-lg hover:shadow-xl cursor-pointer
+              ${flipped ? "animate-flip-to-back" : "animate-flip-to-front"}
+              transition-transform duration-500 ease-in-out transform perspective-1000 overflow-hidden`}
+			onClick={toggleFlip}
+		>
+			{/* Front of the card */}
+			<div className={`w-full h-full flex flex-col justify-between ${flipped ? "hidden" : ""}`}>
+				<img className="flex-grow w-full h-full object-cover rounded-t-lg" src={imageUrl} alt={word} />
+			</div>
+
+			{/* Back of the card */}
+			<div
+				className={`w-full h-full flex flex-col justify-between
+              ${flipped ? "animate-flip-to-back" : "animate-flip-to-front"}
+              transition-transform duration-500 ease-in-out transform perspective-1000
+              ${flipped ? "rotate-y-180" : "rotate-y-0 hidden"}`}
+			>
+				<div className={`h-full transform ${flipped ? "rotate-y-180" : ""}`}>
+					<div className="flex flex-col h-full">
+						<div className="flex-grow flex justify-center items-center text-center p-4 bg-gray-200">
+							<span className="font-bold text-4xl">{word}</span>
+						</div>
+						<div className="flex-shrink-0 px-6 py-4">
+							<div className="font-bold text-xl text-center">{translation}</div>
+							<audio ref={audioRef} src={audioUrl} controls>
+								Your browser does not support the audio element.
+							</audio>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default FlashCard;
--- frontend/src/components/Pages/LessonsPage/VocabularyPage.tsx
+++ frontend/src/components/Pages/LessonsPage/VocabularyPage.tsx
@@ -1,46 +1,94 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { FaRegNewspaper } from "react-icons/fa";
 import Layout from "../../Layout/Layout";
 import NotFoundPage from "../NotFoundPage";
 import FlashCard from "../../Items/Misc/FlashCard";
+import { TbVocabulary } from "react-icons/tb";
 
 const VocabularyPage: React.FC = () => {
-    const { lesson_number } = useParams<{ lesson_number: string }>();
-    const [error, setError] = useState<string | null>(null);
+	const { lesson_number } = useParams<{ lesson_number: string }>();
+	const [error, setError] = useState<string | null>(null);
 
-    const flashCardsData = [
-        {
-            word: 'Strand',
-            translation: 'Beach',
-            audioUrl: '/path/to/beach-audio.mp3',
-            imageUrl: '/src/assets/courses/german/_shared/lessons/lesson7/images/beach.png',
-        },
-    ];
+	const flashCardsData = [
+		{
+			word: "Das Brot",
+			translation: "The Bread",
+			audioUrl: "/src/assets/courses/german/_shared/lessons/lesson7/audio/Das Brot.mp3",
+			imageUrl: "/src/assets/courses/german/_shared/lessons/lesson7/images/bread.png",
+		},
+		{
+			word: "Das Wasser",
+			translation: "The Water",
+			audioUrl: "/src/assets/courses/german/_shared/lessons/lesson7/audio/Das Wasser.mp3",
+			imageUrl: "/src/assets/courses/german/_shared/lessons/lesson7/images/water.png",
+		},
+		{
+			word: "Der Kaffee",
+			translation: "The Coffee",
+			audioUrl: "/src/assets/courses/german/_shared/lessons/lesson7/audio/Der Kaffee.mp3",
+			imageUrl: "/src/assets/courses/german/_shared/lessons/lesson7/images/coffee.png",
+		},
+		{
+			word: "Der Apfel",
+			translation: "The Apple",
+			audioUrl: "/src/assets/courses/german/_shared/lessons/lesson7/audio/Der Apfel.mp3",
+			imageUrl: "/src/assets/courses/german/_shared/lessons/lesson7/images/apple.png",
+		},
+		{
+			word: "Der Kuchen",
+			translation: "The Cake",
+			audioUrl: "/src/assets/courses/german/_shared/lessons/lesson7/audio/Der Kuchen.mp3",
+			imageUrl: "/src/assets/courses/german/_shared/lessons/lesson7/images/cake.png",
+		},
+		{
+			word: "Das Bier",
+			translation: "The Beer",
+			audioUrl: "/src/assets/courses/german/_shared/lessons/lesson7/audio/Das Bier.mp3",
+			imageUrl: "/src/assets/courses/german/_shared/lessons/lesson7/images/beer.png",
+		},
+		{
+			word: "Der Käse",
+			translation: "The Cheese",
+			audioUrl: "/src/assets/courses/german/_shared/lessons/lesson7/audio/Der Käse.mp3",
+			imageUrl: "/src/assets/courses/german/_shared/lessons/lesson7/images/cheese.png",
+		},
+		{
+			word: "Die Suppe",
+			translation: "The Soup",
+			audioUrl: "/src/assets/courses/german/_shared/lessons/lesson7/audio/Die Suppe.mp3",
+			imageUrl: "/src/assets/courses/german/_shared/lessons/lesson7/images/soup.png",
+		},
+		{
+			word: "Der Wein",
+			translation: "The Wine",
+			audioUrl: "/src/assets/courses/german/_shared/lessons/lesson7/audio/Der Wine.mp3",
+			imageUrl: "/src/assets/courses/german/_shared/lessons/lesson7/images/wine.png",
+		},
+	];
 
-    useEffect(() => {
-        // Fetch data or perform other setup tasks
-    }, [lesson_number]);
+	useEffect(() => {
+		// Fetch data or perform other setup tasks
+	}, [lesson_number]);
 
-    return (
-        <Layout>
-            {error ? (
-                <NotFoundPage />
-            ) : (
-                <>
-                    <h2 className="text-2xl font-bold mb-6">Vocabulary for Lesson {lesson_number}</h2>
-                    <div className="flex justify-center text-3xl mb-6">
-                        <FaRegNewspaper />
-                    </div>
-                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-                        {flashCardsData.map((card, index) => (
-                            <FlashCard key={index} {...card} />
-                        ))}
-                    </div>
-                </>
-            )}
-        </Layout>
-    );
+	return (
+		<Layout>
+			{error ? (
+				<NotFoundPage />
+			) : (
+				<>
+					<h2 className="text-2xl font-bold mb-6">Vocabulary for Lesson {lesson_number}</h2>
+					<div className="flex justify-center text-3xl mb-6">
+						<TbVocabulary />
+					</div>
+					<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+						{flashCardsData.map((card, index) => (
+							<FlashCard key={index} {...card} />
+						))}
+					</div>
+				</>
+			)}
+		</Layout>
+	);
 };
 
 export default VocabularyPage;
