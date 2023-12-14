eba88e1ad5fb52941fd9ebf95ecce4402785a785
Irving Zamora <irvingzamora28@gmail.com>
2023-12-14 06:30:43 -0600

    test: Add Unit test suit for VocabularyExerciseControllerTest
    - Add comprehensive unit tests in `VocabularyExerciseControllerTest`. These tests cover various scenarios including valid and invalid lesson number requests, and cases with non-existing courses or lessons. The tests ensure the robustness and reliability of the `VocabularyExerciseController` functionality.
    
    - Update `VocabularyExerciseController` to include detailed doc-blocks for methods, enhancing code readability and documentation.
    
    - Create model factories for `Course`, `Lesson`, and `VocabularyExercise`

--- app/Http/Controllers/VocabularyExerciseController.php
+++ app/Http/Controllers/VocabularyExerciseController.php
@@ -11,8 +11,22 @@ use App\Models\Lesson;
 use App\Models\VocabularyExercise;
 use Illuminate\Http\Request;
 
+/**
+ * Class VocabularyExerciseController
+ * 
+ * This class is responsible for handling vocabulary exercise related requests.
+ */
 class VocabularyExerciseController extends Controller
 {
+    /**
+     * Get vocabulary exercises by lesson number.
+     *
+     * @param int $lessonNumber The lesson number.
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection The collection of vocabulary exercises.
+     * @throws InvalidAPIParameterException If the lesson number is invalid.
+     * @throws CourseNotFoundException If the course is not found.
+     * @throws LessonNotFoundException If the lesson is not found.
+     */
     public function getByLessonNumber($lessonNumber)
     {
         if (!is_numeric($lessonNumber)) {
--- app/Models/VocabularyExercise.php
+++ app/Models/VocabularyExercise.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Jenssegers\Mongodb\Eloquent\Model;
 
 class VocabularyExercise extends Model
 {
+    use HasFactory;
+
     protected $collection = 'vocabulary_exercises';
     protected $fillable = ['prompt', 'options', 'answer', 'lesson_id'];
 
