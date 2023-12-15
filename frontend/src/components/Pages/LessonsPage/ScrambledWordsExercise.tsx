5f6d7f29d0083d6fd625c8efd4e3e7529fa52616
Irving Zamora <irvingzamora28@gmail.com>
2023-12-15 00:20:23 -0600

    feat: Add back button in Scrambled words exercise (welcome section) - Implement BEM Naming Convention in Scrambled Words Exercise UI (welcome section only)

--- frontend/src/components/Pages/LessonsPage/ScrambledWordsExercise.tsx
+++ frontend/src/components/Pages/LessonsPage/ScrambledWordsExercise.tsx
@@ -178,17 +178,25 @@ const ScrambledWordsExercise: React.FC = () => {
 
 	const renderWelcomeScreen = () => (
 		<>
-			<div className="text-center p-4 mb-6 bg-white shadow-md rounded-md">
-				<h1 className="font-bold text-2xl mb-2">Welcome to Scrambled Words!</h1>
-				<p className="mb-4">Click on the letters to form the correct word based on the translation provided.</p>
+			<div className="welcome-section text-center p-4 mb-6 bg-white shadow-md rounded-md">
+				<h1 className="welcome-section__title font-bold text-2xl mb-2">Welcome to Scrambled Words!</h1>
+				<p className="welcome-section__description mb-4">Click on the letters to form the correct word based on the translation provided.</p>
 			</div>
-			<div className="flex flex-col items-center justify-center min-h-[calc(100vh-18rem)] sm:min-h-[calc(100vh-17rem)] bg-gray-100">
+			<div className="back-button-section flex place-content-center sm:place-content-end">
+				<Link
+					to={`/lessons/${lesson_number}/exercises`}
+					className="back-button-section__link flex items-center w-fit border border-primary-500 text-primary-500 hover:bg-primary-500 hover:text-white font-bold py-2 px-4 my-2 rounded-lg shadow sm:col-start-3 sm:col-end-4 order-last sm:order-none"
+				>
+					<MdArrowBack className="back-button-section__icon mr-2" /> Back to exercises
+				</Link>
+			</div>
+			<div className="start-button-section flex flex-col items-center justify-center min-h-[calc(100vh-18rem)] sm:min-h-[calc(100vh-17rem)] bg-gray-100">
 				<button
-					className="items-center mx-auto shadow-box justify-center h-24 w-64 sm:h-20 sm:w-56 drop-shadow-xl rounded-lg px-8 py-4 overflow-hidden group bg-yellow-400 relative hover:bg-gradient-to-r from-yellow-400 to-yellow-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-yellow-400 transition-all ease-out duration-300"
+					className="start-button-section__cta items-center mx-auto shadow-box justify-center h-24 w-64 sm:h-20 sm:w-56 drop-shadow-xl rounded-lg px-8 py-4 overflow-hidden group bg-yellow-400 relative hover:bg-gradient-to-r from-yellow-400 to-yellow-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-yellow-400 transition-all ease-out duration-300"
 					onClick={() => updateState({ gameStarted: true })}
 				>
-					<span className="absolute right-0 w-12 h-44 -mt-12 sm:w-8 sm:h-32 sm:-mt-8 transition-all duration-1000 transform translate-x-16 bg-white opacity-10 rotate-12 group-hover:-translate-x-72 ease"></span>
-					<span className="relative gender_duel__text-shadow text-4xl sm:text-3xl font-bold">{t("start")}</span>
+					<span className="start-button-section__effect absolute right-0 w-12 h-44 -mt-12 sm:w-8 sm:h-32 sm:-mt-8 transition-all duration-1000 transform translate-x-16 bg-white opacity-10 rotate-12 group-hover:-translate-x-72 ease"></span>
+					<span className="start-button-section__text relative gender_duel__text-shadow text-4xl sm:text-3xl font-bold">{t("start")}</span>
 				</button>
 			</div>
 		</>
