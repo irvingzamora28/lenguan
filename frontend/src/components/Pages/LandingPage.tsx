3fdabaac2b53e0fbd54ffb837e318933ab40901c
Irving Zamora <irvingzamora28@gmail.com>
2023-12-18 07:04:00 -0600

    feat: Refactor Testimonials into Separate Constants File - Move testimonials data from `LandingPage` to `testimonials.ts` in constants - Import and use testimonials data from new constants file in `LandingPage`

--- frontend/src/components/Pages/LandingPage.tsx
+++ frontend/src/components/Pages/LandingPage.tsx
@@ -1,73 +1,10 @@
 import React from "react";
 import "../../assets/scss/pages/LandingPage.scss";
-import { TestimonialCardProps } from "../../types/props";
 import TestimonialCard from "../Items/Cards/TestimonialCard";
 import TestimonialCarousel from "../Items/Misc/TestimonialCarousel";
+import { testimonials } from "../../constants/testimonials";
 
 const LandingPage: React.FC = () => {
-	const testimonials: TestimonialCardProps[] = [
-		{
-			image: "src/assets/images/testimonial-david.png",
-			name: "David Garcia",
-			designation: "Entrepreneur",
-			testimonial: "Integrating language learning into my busy schedule has been seamless with Lenguan. Highly effective and flexible.",
-		},
-		{
-			image: "src/assets/images/testimonial-gloria.png",
-			name: "Gloria Bell",
-			designation: "Professional Translator",
-			testimonial: "The variety of exercises and challenges keeps me engaged and learning every day.",
-		},
-		{
-			image: "src/assets/images/testimonial-default-woman.png",
-			name: "Anna Lee",
-			designation: "Language Teacher",
-			testimonial: "I recommend Lenguan to all my students. It's a great resource for learning languages.",
-		},
-		{
-			image: "src/assets/images/testimonial-carlos.png",
-			name: "Carlos Torres",
-			designation: "Student",
-			testimonial: "As a beginner, I found Lenguan's lessons easy to follow and very practical.",
-		},
-		{
-			image: "src/assets/images/testimonial-default-man.png",
-			name: "Sam Anderson",
-			designation: "Language Learner",
-			testimonial: "I've tried many language learning apps and Lenguan is by far the best. I enjoy so much the challenges!",
-		},
-		{
-			image: "src/assets/images/testimonial-sarah.png",
-			name: "Sarah Jenkins",
-			designation: "Travel Blogger",
-			testimonial: "Lenguan makes it so easy to pick up new languages. I've used it in my travels and it's been a game changer!",
-		},
-		{
-			image: "src/assets/images/testimonial-richard.png",
-			name: "Richard Cooper",
-			designation: "Language Enthusiast",
-			testimonial: "Lenguan has transformed my language learning journey. It's interactive and fun!",
-		},
-		{
-			image: "src/assets/images/testimonial-emily.png",
-			name: "Emily Patel",
-			designation: "Freelance Writer",
-			testimonial: "The cultural insights in Lenguan's lessons are incredible. Learning languages has never been more engaging and informative.",
-		},
-		{
-			image: "src/assets/images/testimonial-mike.png",
-			name: "Mike Johnson",
-			designation: "Software Developer",
-			testimonial: "Lenguan's tech-savvy approach to language learning is perfect for someone like me who loves using the latest apps and tools.",
-		},
-		{
-			image: "src/assets/images/testimonial-default-woman.png",
-			name: "Lisa Chang",
-			designation: "Digital Nomad",
-			testimonial: "With Lenguan, I can learn on the go, which is perfect for my lifestyle. It's been an invaluable resource in my travels.",
-		},
-	];
-
 	return (
 		<div className="">
 			<header className="flex flex-wrap justify-between items-center p-6 md:flex-row flex-col">
