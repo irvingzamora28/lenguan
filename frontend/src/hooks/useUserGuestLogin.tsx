2228507dcf4754e0b06eff1caf6bf1d91a8ed932
Irving Zamora <irvingzamora28@gmail.com>
2023-12-30 16:17:55 -0600

    feat: Assign english as default language for guest users when logging in as guest - Refactor useUserGuestLogin to fetch languages to assign english language

--- frontend/src/hooks/useUserGuestLogin.tsx
+++ frontend/src/hooks/useUserGuestLogin.tsx
@@ -1,16 +1,29 @@
 import { useNavigate } from "react-router-dom";
-import { useAppDispatch } from "../redux/hooks";
+import { useAppDispatch, useCourses, useLanguages } from "../redux/hooks";
 import { generateGuestUser } from "../utils/userUtils";
 import { loginGuest, loginRequest } from "../redux/authSlice";
+import { getLanguages } from "../utils/languages";
 
 const useUserGuestLogin = (path?: string) => {
 	const dispatch = useAppDispatch();
 	const navigate = useNavigate();
+	const languages = useLanguages();
+	const courses = useCourses();
 
-	const handleLoginAsGuest = () => {
-		const guestUser = generateGuestUser();
+	// Fetch the languages available to select
+	getLanguages(languages, dispatch);
+
+	const handleLoginAsGuest = async () => {
 		dispatch(loginRequest());
-		dispatch(loginGuest({ user: guestUser }));
+		const guestUser = generateGuestUser();
+		// Assign english as default language for guest users
+		const englishLanguage = languages?.find((language) => language.name === "English");
+
+		const userPayload = {
+			user: { ...guestUser, language: englishLanguage || undefined },
+		};
+
+		dispatch(loginGuest(userPayload));
 		if (path) {
 			navigate(path);
 		}
