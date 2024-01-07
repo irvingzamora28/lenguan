6d1226127dc9f9a89663657a8d5f2c2496540e8e
Irving Zamora <irvingzamora28@gmail.com>
2024-01-07 16:16:40 -0600

    feat: Update login and register pages styling to use responsive max-widths and adjust navigation links - Add header to login and register pages to navigate between them and to the home page

--- frontend/src/components/Pages/AuthPage/LoginPage.tsx
+++ frontend/src/components/Pages/AuthPage/LoginPage.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import loginImage from "./../../../assets/images/login-image.png";
 import "../../../assets/scss/components/LoginPage.scss";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import LoginForm from "./../../Items/Forms/LoginForm";
 import { LoginService } from "./../../../services/LoginService";
 import useUserGuestLogin from "../../../hooks/useUserGuestLogin";
@@ -21,22 +21,35 @@ const LoginPage: React.FC = () => {
 	}, []);
 
 	return (
-		<section className="login__section">
-			<div className="login__section-content">
-				<div className="md:flex">
-					<div className="flex md:w-1/2 md:pr-8 justify-center items-center">
-						<div className="flex justify-center md:block">
-							<img className="login__image rounded-full md:rounded-lg shadow-lg max-w-xs lg:max-w-md" src={loginImage} alt="Register" />
+		<>
+			<header className="flex flex-wrap justify-between items-center p-6 md:flex-row">
+				<Link to="/" className="flex items-center">
+					<h1 className="text-2xl font-bold mb-2 md:mb-0 text-primary-800">Lenguan</h1>
+				</Link>
+
+				<div className="flex space-x-4">
+					<Link to="/register" className="bg-primary-500 hover:bg-primary-700 text-white font-bold py-2 px-4 rounded">
+						Register
+					</Link>
+				</div>
+			</header>
+			<section className="login__section">
+				<div className="login__section-content">
+					<div className="md:flex">
+						<div className="flex md:w-1/2 justify-center items-center">
+							<div className="flex justify-center md:block">
+								<img className="login__image rounded-full md:rounded-lg shadow-lg max-w-xs lg:max-w-md" src={loginImage} alt="Register" />
+							</div>
+						</div>
+
+						<div className="md:w-1/2">
+							<h2 className="login__heading">Login to Your Account</h2>
+							<LoginForm onLogin={handleLogin} onChange={handleChange} errorMessages={errorMessages} onLoginAsGuest={handleLoginAsGuest} />
 						</div>
 					</div>
-
-					<div className="md:w-1/2">
-						<h2 className="login__heading">Login to Your Account</h2>
-						<LoginForm onLogin={handleLogin} onChange={handleChange} errorMessages={errorMessages} onLoginAsGuest={handleLoginAsGuest} />
-					</div>
 				</div>
-			</div>
-		</section>
+			</section>
+		</>
 	);
 };
 
--- frontend/src/components/Pages/AuthPage/RegisterPage.tsx
+++ frontend/src/components/Pages/AuthPage/RegisterPage.tsx
@@ -3,27 +3,41 @@ import registerImage from "../../../assets/images/register-image.png";
 import "../../../assets/scss/components/RegisterPage.scss";
 import RegisterForm from "../../Items/Forms/RegisterForm";
 import useUserRegister from "../../../hooks/useUserRegister";
+import { Link } from "react-router-dom";
 
 const RegisterPage: React.FC = () => {
 	const { errorMessages, registerData, handleChange, registerResponse, handleRegister } = useUserRegister("/login");
 
 	return (
-		<section className="register__section">
-			<div className="register__section-content">
-				<div className="md:flex">
-					<div className="flex md:w-1/2 md:pr-8 justify-center items-center">
-						<div className="flex justify-center md:block">
-							<img className="register__image rounded-full md:rounded-lg shadow-lg max-w-xs lg:max-w-md" src={registerImage} alt="Register" />
+		<>
+			<header className="flex flex-wrap justify-between items-center p-6 md:flex-row">
+				<Link to="/" className="flex items-center">
+					<h1 className="text-2xl font-bold mb-2 md:mb-0 text-primary-800">Lenguan</h1>
+				</Link>
+
+				<div className="flex space-x-4">
+					<Link to="/login" className="bg-primary-500 hover:bg-primary-700 text-white font-bold py-2 px-4 rounded">
+						Log in
+					</Link>
+				</div>
+			</header>
+			<section className="register__section">
+				<div className="register__section-content">
+					<div className="md:flex">
+						<div className="flex md:w-1/2 justify-center items-center">
+							<div className="flex justify-center md:block">
+								<img className="register__image rounded-full md:rounded-lg shadow-lg max-w-xs lg:max-w-md" src={registerImage} alt="Register" />
+							</div>
+						</div>
+
+						<div className="md:w-1/2">
+							<h2 className="register__heading">Create your account</h2>
+							<RegisterForm onRegister={handleRegister} onChange={handleChange} registerResponse={registerResponse} registerData={registerData} errorMessages={errorMessages} />
 						</div>
 					</div>
-
-					<div className="md:w-1/2">
-						<h2 className="register__heading">Create your account</h2>
-						<RegisterForm onRegister={handleRegister} onChange={handleChange} registerResponse={registerResponse} registerData={registerData} errorMessages={errorMessages} />
-					</div>
 				</div>
-			</div>
-		</section>
+			</section>
+		</>
 	);
 };
 
