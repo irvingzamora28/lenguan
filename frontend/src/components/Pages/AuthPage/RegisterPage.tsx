e827cdeac40011706668d43a78f722b4b0ab3bf3
Irving Zamora <irvingzamora28@gmail.com>
2024-01-07 14:28:50 -0600

    style: Refactor register page styles for improved responsiveness

--- frontend/src/components/Pages/AuthPage/RegisterPage.tsx
+++ frontend/src/components/Pages/AuthPage/RegisterPage.tsx
@@ -10,13 +10,18 @@ const RegisterPage: React.FC = () => {
 	return (
 		<section className="register__section">
 			<div className="register__section-content">
-				<div>
-					<img className="register__image" src={registerImage} alt="Register" />
-					<div>
+				<div className="md:flex">
+					<div className="flex md:w-1/2 md:pr-8 justify-center items-center">
+						<div className="flex justify-center md:block">
+							<img className="register__image rounded-lg shadow-lg max-w-xs lg:max-w-md" src={registerImage} alt="Register" />
+						</div>
+					</div>
+
+					<div className="md:w-1/2">
 						<h2 className="register__heading">Create your account</h2>
+						<RegisterForm onRegister={handleRegister} onChange={handleChange} registerResponse={registerResponse} registerData={registerData} errorMessages={errorMessages} />
 					</div>
 				</div>
-				<RegisterForm onRegister={handleRegister} onChange={handleChange} registerResponse={registerResponse} registerData={registerData} errorMessages={errorMessages} />
 			</div>
 		</section>
 	);
