25fc80acac1fcc8d6ac0cb398133d54c3d207813
Irving Zamora <irvingzamora28@gmail.com>
2024-01-07 07:46:42 -0600

    feat: Add interceptor to api.ts to set XSRF token before each request

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -4,10 +4,28 @@ import Config from "./config";
 let baseURL = Config.getBaseUrl();
 
 const api = axios.create({
-    baseURL: baseURL,
-    withCredentials: true,
+	baseURL: baseURL,
+	withCredentials: true,
 });
 
+// Use interceptor to set the XSRF token before each request
+api.interceptors.request.use(
+	(config) => {
+		// Ensure this runs in a browser environment
+		if (typeof document !== "undefined") {
+			const tokenElement = document.querySelector('meta[name="csrf-token"]');
+			const token = tokenElement?.getAttribute("content")?.replace("%3D", "=");
+			if (token) {
+				config.headers["X-XSRF-TOKEN"] = token;
+			}
+		}
+		return config;
+	},
+	(error) => {
+		// Do something with request error
+		return Promise.reject(error);
+	}
+);
 axios.defaults.baseURL = baseURL;
 
 export default api;
