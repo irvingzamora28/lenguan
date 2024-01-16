75c6ae911c458329a8ad2371156ca08443a220ba
Irving Zamora <irvingzamora28@gmail.com>
2024-01-16 03:52:43 -0600

    feat: Add email notification for form submission - Configure zoho client to send mail - Introduce email notification to send form submission data - Create email view for contact form submission details

--- app/Http/Controllers/FormSubmitController.php
+++ app/Http/Controllers/FormSubmitController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ContactFormRequest;
+use App\Mail\ContactFormMail;
 use App\Models\FormSubmission;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class FormSubmitController extends Controller
 {
@@ -15,6 +17,7 @@ class FormSubmitController extends Controller
         try {
             $data = $request->validated();
             $formSubmission = FormSubmission::create(['data' => $data]);
+            Mail::to('contact@lenguan.de')->send(new ContactFormMail($data));
             return response()->json(['message' => 'Form submitted successfully', 'success' => true], 200);
         } catch (\Exception $e) {
             return response()->json(['message' => 'Error submitting form: ' . $e->getMessage(), 'success' => false], 500);
---
+++ app/Mail/ContactFormMail.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class ContactFormMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $data;
+
+    /**
+     * Create a new message instance.
+     *
+     * @param  array  $data
+     */
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'New Contact Form Submission',
+            from: new Address(config('mail.from.address'), config('mail.from.name')),
+        );
+    }
+
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.contact',  // Point this to the actual view for your email
+            with: ['data' => $this->data], // Pass data to the view
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        // Add attachments if needed
+        return [];
+    }
+}
