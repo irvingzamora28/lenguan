6b5a6221fa088cdc6a11fcba9524d2e284725fbc
Irving Zamora <irvingzamora28@gmail.com>
2024-01-24 16:00:44 -0600

    feat: Add grammar exercises and seeder for language courses
    - Add a new model "GrammarExercise" to manage grammar exercises and a seeder "GrammarExercisesSeeder" to populate the database with exercises for language courses.

---
+++ app/Models/GrammarExercise.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use MongoDB\Laravel\Eloquent\Model;
+
+class GrammarExercise extends Model
+{
+    use HasFactory;
+
+    protected $collection = 'grammar_exercises';
+    protected $fillable = ['prompt', 'options', 'answer', 'lesson_id'];
+
+    public function lesson()
+    {
+        return $this->belongsTo(Lesson::class);
+    }
+}
