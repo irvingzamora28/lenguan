92a1452fa15d45cd82958ba9144760f475896684
Irving Zamora <irvingzamora28@gmail.com>
2024-01-24 05:39:21 -0600

    feat: Improve flashcard flip animation and dynamic content height adjustment on flip.

--- frontend/src/components/Items/Lesson/FlashCardVocabulary.tsx
+++ frontend/src/components/Items/Lesson/FlashCardVocabulary.tsx
@@ -1,6 +1,5 @@
-// FlashcardVocabulary.tsx
-import React, { useState } from "react";
-import { MdTranslate, MdRecordVoiceOver } from "react-icons/md"; // Assuming you're using react-icons
+import React, { useState, useRef, useEffect } from "react";
+import { MdTranslate, MdRecordVoiceOver } from "react-icons/md";
 
 type Gender = "fem" | "masc" | "neut";
 
@@ -14,6 +13,15 @@ interface FlashcardVocabularyProps {
 
 const FlashcardVocabulary: React.FC<FlashcardVocabularyProps> = ({ word, translation, exampleSentence, exampleTranslation, gender }) => {
 	const [isFlipped, setIsFlipped] = useState(false);
+	const [backCardHeight, setBackCardHeight] = useState("h-24");
+	const backCardContentRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		if (isFlipped && backCardContentRef.current) {
+			const contentHeight = backCardContentRef.current.scrollHeight;
+			setBackCardHeight(`h-[${contentHeight}px]`);
+		}
+	}, [isFlipped]);
 
 	const genderColor = {
 		fem: "bg-red-100 text-red-700 dark:bg-red-700 dark:text-red-50",
@@ -27,9 +35,9 @@ const FlashcardVocabulary: React.FC<FlashcardVocabularyProps> = ({ word, transla
 
 	return (
 		<div
-			className={`relative w-full max-w-md mx-auto h-24 shadow-lg rounded-xl hover:shadow-xl cursor-pointer
-        ${isFlipped ? "animate-flip-to-back" : "animate-flip-to-front"}
-        transition-transform duration-500 ease-in-out transform perspective-1000 overflow-hidden`}
+			className={`relative w-full max-w-md mx-auto shadow-lg rounded-xl hover:shadow-xl cursor-pointer
+            ${isFlipped ? "animate-flip-to-back" : "animate-flip-to-front"}
+            transition-transform duration-500 ease-in-out transform perspective-1000 overflow-hidden`}
 			onClick={handleFlip}
 		>
 			{/* Front of the card */}
@@ -47,13 +55,13 @@ const FlashcardVocabulary: React.FC<FlashcardVocabularyProps> = ({ word, transla
 
 			{/* Back of the card */}
 			<div
-				className={`w-full h-full flex flex-col justify-around p-6 items-center text-center bg-slate-100 text-gray-800 dark:bg-slate-700 dark:text-slate-400
-        ${isFlipped ? "animate-flip-to-back" : "animate-flip-to-front"}
-        transition-transform duration-500 ease-in-out transform perspective-1000
-        ${isFlipped ? "rotate-y-180" : "rotate-y-0 hidden"}`}
+				className={`w-full ${backCardHeight} flex flex-col justify-around p-6 items-center text-center bg-slate-100 text-gray-800 dark:bg-slate-700 dark:text-slate-400
+                ${isFlipped ? "animate-flip-to-back" : "animate-flip-to-front"}
+                transition-transform duration-500 ease-in-out transform perspective-1000
+                ${isFlipped ? "rotate-y-180" : "rotate-y-0 hidden"} transition-all duration-500 ease-in-out`}
 			>
 				{/* This div is rotated to make sure the text appears the right way up */}
-				<div className={`transform ${isFlipped ? "rotate-y-180" : ""}`}>
+				<div className={`transform ${isFlipped ? "rotate-y-180" : ""}`} ref={backCardContentRef}>
 					<div className="grid grid-cols-[auto_1fr] items-center">
 						{/* Icon and Translation */}
 						<MdRecordVoiceOver className="text-md" />
